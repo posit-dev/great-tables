b0cc183b817cc02899e4105a32a3edeca67bcd46
Richard Iannone <riannone@me.com>
2022-06-06 11:49:58 -0400

    Update _boxhead.py

--- gt/_boxhead.py
+++ gt/_boxhead.py
@@ -1,37 +1,58 @@
-import pandas as pd
-import numpy as np
+from typing import Optional
+from enum import Enum
+
+from ._base_api import BaseAPI
+from ._tbl_data import TblData
+
+
+class ColumnAlignment(Enum):
+    Left = 1
+    Center = 2
+    Right = 3
+    Justify = 4
+
+
+class ColInfo:
+    # TODO: Make var readonly
+    var: str
+    column_label: str
+    visible: bool
+    column_align: Optional[ColumnAlignment]
+    column_width: Optional[str]
+
+    # The components of the boxhead are:
+    # `var` (obtained from column names)
+    # `column_label` (obtained from column names)
+    # `visible` = True
+    # `column_align` = None
+    # `column_width` = None
+
+    def __init__(
+        self,
+        var: str,
+        column_label: Optional[str] = None,
+        visible: bool = True,
+        column_align: Optional[ColumnAlignment] = None,
+        column_width: Optional[str] = None,
+    ):
+        self.var = var
+        self.column_label = column_label or var
+        self.visible = visible
+        self.column_align = column_align
+        self.column_width = column_width
 
 
 class Boxhead:
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self, data: TblData):
 
-        # Transform incoming data to a pandas DataFrame
-        # TODO: use already stored data in `_tbl_data` in `GT()`
-        pd_data = pd.DataFrame(data)
-        column_names = list(pd_data.columns)
-
-        # The `boxhead` DataFrame is used to handle column labels, column
-        # ordering, alignments of entire columns, column widths, and
-        # column visibility (e.g., displayed/hidden)
-        # 0: `var` (obtained from column names)
-        # 1: `type` = "default"
-        # 2: `column_label` (obtained from column names)
-        # 3: `column_align` = "center"
-        # 4: `column_width` = np.nan
-
-        self._boxhead: pd.DataFrame = pd.DataFrame(
-            {
-                "var": column_names,
-                "type": "default",
-                "column_label": column_names,
-                "column_align": "center",
-                "column_width": np.nan,
-            }
-        )
+        # Obtain the column names from the data and initialize the
+        # `_boxhead` from that
+        column_names = data.columns
+        self._boxhead: list[ColInfo] = [ColInfo(col) for col in column_names]
 
 
-class BoxheadAPI:
+class BoxheadAPI(BaseAPI):
     _boxhead: Boxhead
 
-    def __init__(self, data: pd.DataFrame):
-        self._boxhead = Boxhead(data)
+    def __init__(self):
+        self._boxhead = Boxhead(self._data)
