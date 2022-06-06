06eed24ed4e4d1b6bf1fbd7ea7b6f9cc1647752a
Richard Iannone <riannone@me.com>
2022-06-06 16:25:25 -0400

    Update _stub.py

--- gt/_stub.py
+++ gt/_stub.py
@@ -1,39 +1,53 @@
-import pandas as pd
-import numpy as np
+from typing import Optional
+
+from ._base_api import BaseAPI
+from ._tbl_data import TblData
+
+
+class RowInfo:
+    # TODO: Make `rownum_i` readonly
+    rownum_i: int
+    group_id: Optional[str]
+    rowname: Optional[str]
+    group_label: Optional[str]
+    built: bool = False
+
+    # The components of the stub are:
+    # `rownum_i` (The initial row indices for the table at ingest time)
+    # `group_id` = None
+    # `rowname` = None
+    # `group_label` = None
+    # `built` = False
+
+    def __init__(
+        self,
+        rownum_i: int,
+        group_id: Optional[str] = None,
+        rowname: Optional[str] = None,
+        group_label: Optional[str] = None,
+        built: bool = False,
+    ):
+        self.rownum_i = rownum_i
+        self.group_id = group_id
+        self.rowname = rowname
+        self.group_label = group_label
+        self.built = built
 
 
 class Stub:
-    def __init__(self, data: pd.DataFrame):
+    def __init__(self, data: TblData):
 
-        # Transform incoming data to a pandas DataFrame
-        # TODO: use already stored data in `_tbl_data` in `GT()`
-        pd_data = pd.DataFrame(data)
+        # Obtain a list of row indices from the data and initialize
+        # the `_stub` from that
+        row_indices = list(range(data.rows))
 
-        row_indices = list(range(len(pd_data)))
-
-        # The `stub` DataFrame is used to handle row indices,
-        # row labels, any associated groups, and built labels
-        # for the output context
-
-        # 0: rownum_i = obtained from length of DF
-        # 1: group_id = np.nan
-        # 2: rowname = np.nan
-        # 3: group_label = np.nan
-        # 4: built = np.nan
-
-        self._stub: pd.DataFrame = pd.DataFrame(
-            {
-                "rownum_i": row_indices,
-                "group_id": np.nan,
-                "rowname": np.nan,
-                "group_label": np.nan,
-                "built": np.nan,
-            }
-        )
+        # Obtain the column names from the data and initialize the
+        # `_boxhead` from that
+        self._stub: list[RowInfo] = [RowInfo(col) for col in row_indices]
 
 
-class StubAPI:
+class StubAPI(BaseAPI):
     _stub: Stub
 
-    def __init__(self, data: pd.DataFrame):
-        self._stub = Stub(data)
+    def __init__(self):
+        self._stub = Stub(self._data)
