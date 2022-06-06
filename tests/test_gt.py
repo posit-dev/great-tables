97f47c68adb76bc8ba416562c188bea7cfd460d9
Richard Iannone <riannone@me.com>
2022-06-06 16:58:15 -0400

    Create `test_gt.py` and add simple test

--- tests/test_gt.py
+++ tests/test_gt.py
@@ -1,1 +1,9 @@
-from gt import gt
+from gt import *
+import pandas as pd
+
+
+def test_gt_object():
+    data = [{"a": 5, "b": 15}, {"a": 15, "b": 2000}]
+    pd_data = pd.DataFrame(data)
+    gt_tbl = gt.GT(pd_data)
+    assert type(gt_tbl).__name__ == "GT"
