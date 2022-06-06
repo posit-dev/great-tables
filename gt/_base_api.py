0f851b7671b55b883cc5734235e4ba732a225a7a
Richard Iannone <riannone@me.com>
2022-06-06 10:35:45 -0400

    Create _base_api.py

---
+++ gt/_base_api.py
@@ -0,0 +1,17 @@
+from typing import cast
+
+from ._tbl_data import TblData, TblDataAPI
+
+
+class BaseAPI:
+    # It's important that we never keep state in this class, as
+    # we will be multiply inheriting from it. I think. --jcheng
+
+    @property
+    def _data(self) -> TblData:
+        result = cast(TblDataAPI, self)._tbl_data
+        if result is None:
+            raise AssertionError(
+                "BaseAPI._data was called before TblDataAPI was initialized"
+            )
+        return result
