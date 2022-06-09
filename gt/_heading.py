9bdc6e8a6b01cc93586e6e88b3d3575806aee39d
Richard Iannone <riannone@me.com>
2022-06-09 16:35:05 -0400

    Add util fns for title/subtitle existence

--- gt/_heading.py
+++ gt/_heading.py
@@ -1,6 +1,20 @@
 from typing import Optional
 
 
+def heading_has_title(title: Optional[str]) -> bool:
+    if title is None:
+        return False
+    else:
+        return True
+
+
+def heading_has_subtitle(subtitle: Optional[str]) -> bool:
+    if subtitle is None:
+        return False
+    else:
+        return True
+
+
 class Heading:
     title: Optional[str] = None
     subtitle: Optional[str] = None
