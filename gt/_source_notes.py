d6964589467d0fdf041c689a8e40789501d89652
Richard Iannone <riannone@me.com>
2022-06-09 10:49:33 -0400

    Add the `tab_source_note()` method

--- gt/_source_notes.py
+++ gt/_source_notes.py
@@ -2,7 +2,7 @@ from typing import Optional, List
 
 
 class SourceNotes:
-    source_notes: Optional[List[str]]
+    source_notes: Optional[List[str]] = []
 
     def __init__(self):
         pass
@@ -13,3 +13,12 @@ class SourceNotesAPI:
 
     def __init__(self):
         self._source_notes = SourceNotes()
+
+    def tab_source_note(self, source_note: str):
+
+        if self._source_notes.source_notes == []:
+            self._source_notes.source_notes = [source_note]
+        elif type(self._source_notes.source_notes) is list:
+            self._source_notes.source_notes.append(source_note)
+
+        return self
