1fdb846213a920682e12aae49f277ed1f7803f17
Richard Iannone <riannone@me.com>
2022-06-10 13:20:47 -0400

    Update _boxhead.py

--- gt/_boxhead.py
+++ gt/_boxhead.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from enum import Enum, auto
 
 from ._base_api import BaseAPI
@@ -49,6 +49,30 @@ class Boxhead:
         # `_boxhead` from that
         column_names = data.columns
         self._boxhead: list[ColInfo] = [ColInfo(col) for col in column_names]
+
+    # Get a list of visible columns in
+    def _get_visible_columns(self) -> List[str]:
+
+        visible_columns = [x.var for x in self._boxhead if x.visible is True]
+        return visible_columns
+
+    # Get the number of columns for the visible (not hidden) data; this
+    # excludes the number of columns required for the table stub
+    def _get_number_of_visible_data_columns(self) -> int:
+        return len(self._get_visible_columns())
+
+    # Obtain the number of visible columns in the built table; this should
+    # account for the size of the stub in the final, built table
+    def _get_effective_number_of_columns(self) -> int:
+
+        n_data_cols = self._get_number_of_visible_data_columns()
+
+        # TODO: Once the stub is defined in the package, we need to account
+        # for the width of the stub at build time to fully obtain the number
+        # of visible columns in the built table
+        # n_data_cols = n_data_cols + len(get_stub_layout(data=data))
+
+        return n_data_cols
 
 
 class BoxheadAPI(BaseAPI):
