55a7e5ac174e090a63f1977cd852048cf411b946
Richard Iannone <riannone@me.com>
2022-06-10 15:26:18 -0400

    Add basic docstring for `tab_header()`

--- gt/_heading.py
+++ gt/_heading.py
@@ -19,6 +19,28 @@ class HeadingAPI:
         subtitle: Optional[str] = None,
         preheader: Optional[str] = None,
     ):
+        """
+        Add a table header
+
+        Parameters
+        ----------
+        title,subtitle
+            Text to be used in the table title and, optionally, for
+            the table subtitle. We can elect to use the `md()` and `html()`
+            helper functions to style the text as Markdown or to retain HTML
+            elements in the text.
+        preheader
+            Optional preheader content that is rendered above the table. Can
+            be supplied as a list of strings.
+
+        Examples:
+        ---------
+            >>> from gt import *
+            >>> x = GT([{"a": 5, "b": 10}, {"a": 15, "b": 20
+            >>>     .tab_header(title="Title", subtitle="Subtitle")
+            >>> x
+            >>> print(x)
+        """
         self._heading.title = title
         self._heading.subtitle = subtitle
         self._heading.preheader = preheader
