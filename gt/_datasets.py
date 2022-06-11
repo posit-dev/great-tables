0ff88240df2537796fb16686d2bbd5b2331dcfc2
Richard Iannone <riannone@me.com>
2022-06-11 02:00:34 -0400

    Add several TODOs

---
+++ gt/_datasets.py
@@ -0,0 +1,11 @@
+# TODO: Add the `countrypops` dataset
+
+# TODO: Add the `sza` dataset
+
+# TODO: Add the `gtcars` dataset
+
+# TODO: Add the `sp500` dataset
+
+# TODO: Add the `pizzaplace` dataset
+
+# TODO: Add the `exibble` dataset
