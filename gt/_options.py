fc0f17972fdb4d6af223f371dc46386c4bab06e5
Richard Iannone <riannone@me.com>
2022-06-13 18:01:36 -0400

    Add the `_get_option_type()` method

--- gt/_options.py
+++ gt/_options.py
@@ -221,6 +221,10 @@ class Options:
         # TODO: ensure error if we pop from empty list
         return [x for x in range(len(self._options)) if self._options[x].parameter == option].pop() 
 
+    def _get_option_type(self, option: str) -> Union[Any, List[str]]:
+        # TODO: ensure error if we pop from empty list
+        return [x.type for x in self._options if x.parameter == option].pop()
+
     def _get_option_value(self, option: str) -> Union[Any, List[str]]:
         # TODO: ensure error if we pop from empty list
         return [x.value for x in self._options if x.parameter == option].pop()
