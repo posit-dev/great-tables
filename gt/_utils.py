bf3eefad2882b0f7e0823c7d7f9175b586609f4a
Richard Iannone <riannone@me.com>
2022-06-14 00:44:20 -0400

    Add `_match_arg()` util fn

--- gt/_utils.py
+++ gt/_utils.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 
 def heading_has_title(title: Optional[str]) -> bool:
@@ -13,3 +13,9 @@ def heading_has_subtitle(subtitle: Optional[str]) -> bool:
         return False
     else:
         return True
+
+
+# TODO: generate informative user-facing error message if argument value is not matched
+# TODO: require that `lst` contains unique elements
+def _match_arg(x: str, lst: List[str]):
+    return [el for el in lst if x in el]
