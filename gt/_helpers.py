c363527269df9d7d5498ddbaadd78a52359c79e7
Richard Iannone <riannone@me.com>
2022-06-13 23:51:52 -0400

    Add letter-list helpers

---
+++ gt/_helpers.py
@@ -0,0 +1,78 @@
+from typing import Union, List
+
+
+def letters() -> List[str]:
+    """Lowercase letters of the Roman alphabet
+
+    Returns:
+        List[str]: the 26 lowercase letters of the Roman alphabet
+    """
+    lett = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+
+    return lett
+
+
+def LETTERS() -> List[str]:
+    """Uppercase letters of the Roman alphabet
+
+    Returns:
+        List[str]: the 26 uppercase letters of the Roman alphabet
+    """
+    lett = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ]
+
+    return lett
+
