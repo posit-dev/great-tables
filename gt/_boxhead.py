515ffac20b1c3e360f7ef6047c1eeb6fbe36f480
Richard Iannone <riannone@me.com>
2022-06-15 18:05:46 -0400

    Add the `cols_label()` method

--- gt/_boxhead.py
+++ gt/_boxhead.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 
 from ._base_api import BaseAPI
 from ._tbl_data import TblData
+from gt import _utils
 
 
 class ColumnAlignment(Enum):
@@ -50,6 +51,23 @@ class Boxhead:
         column_names = data.columns
         self._boxhead: list[ColInfo] = [ColInfo(col) for col in column_names]
 
+    # Get a list of columns
+    def _get_columns(self) -> List[str]:
+        return [x.var for x in self._boxhead]
+
+    # Get a list of column labels
+    def _get_column_labels(self) -> List[str]:
+        return [x.column_label for x in self._boxhead]
+
+    # Set column label
+    def _set_column_label(self, column: str, label: str):
+
+        for x in self._boxhead:
+            if x.var == column:
+                x.column_label = label
+
+        return self
+
     # Get a list of visible columns
     def _get_visible_columns(self) -> List[str]:
 
@@ -80,3 +98,51 @@ class BoxheadAPI(BaseAPI):
 
     def __init__(self):
         self._boxhead = Boxhead(self._data)
+
+    def cols_label(self, **kwargs: str):
+        """
+        Relabel one or more columns.
+
+        Column labels can be modified from their default values (the names of the
+        columns from the input table data). When you create a gt table object
+        using `gt.GT()`, column names effectively become the column labels. While
+        this serves as a good first approximation, column names aren't often
+        appealing as column labels in a gt output table. The `cols_label()` method
+        provides the flexibility to relabel one or more columns and we even have the
+        option to use the `md()` or `html()` helper functions for rendering column
+        labels from Markdown or using HTML.
+
+        It's important to note that while columns can be freely relabeled, we
+        continue to refer to columns by their original column names. Column names in
+        a tibble or data frame must be unique whereas column labels in gt have
+        no requirement for uniqueness (which is useful for labeling columns as, say,
+        measurement units that may be repeated several times---usually under
+        different spanner column labels). Thus, we can still easily distinguish
+        between columns in other gt method calls (e.g., in all of the `fmt*()`
+        methods) even though we may lose distinguishability in column labels once
+        they have been relabeled.
+
+        Returns
+        -------
+        GT
+            Result of the table operation.
+        """
+
+        # If nothing is provided, return `data` unchanged
+        if len(kwargs) == 0:
+            return self
+
+        mod_columns = list(kwargs.keys())
+        new_labels = list(kwargs.values())
+
+        # Get the full list of column names for the data
+        column_names = self._boxhead._get_columns()
+
+        # Stop function if any of the column names specified are not in `cols_labels`
+        # msg: "All column names provided must exist in the input `.data` table."
+        _utils._assert_list_is_subset(mod_columns, column_names)
+
+        for i in range(len(kwargs)):
+            self._boxhead._set_column_label(column=mod_columns[i], label=new_labels[i])
+
+        return self
