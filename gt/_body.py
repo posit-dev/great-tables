120a3cd8399dbb94a0cb5dd1327722ab8f3e35be
Richard Iannone <riannone@me.com>
2022-06-21 10:23:46 -0400

    Create _body.py

---
+++ gt/_body.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+
+class Body:
+    body: Optional[List[List[str]]]
+
+    def __init__(self):
+        pass
+
+
+class BodyAPI:
+    _body: Body
+
+    def __init__(self):
+        pass
