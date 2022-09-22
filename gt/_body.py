3fba1371e5a4567c5116b64011b99b550138e0fb
Richard Iannone <riannone@me.com>
2022-09-22 17:24:10 -0400

    Update _body.py

--- gt/_body.py
+++ gt/_body.py
@@ -1,18 +1,48 @@
-from typing import List, Optional, Union
+from typing import Optional, Union
 import pandas as pd
+from ._tbl_data import TblData
 
 
 class Body:
-    body: pd.DataFrame
+    body: TblData
     data: Optional[int]
 
-    def __init__(self, body: pd.DataFrame, data: Optional[int] = None):
-        self.body = body
+    def __init__(self, body: Union[pd.DataFrame, TblData], data: Optional[int] = None):
+        if isinstance(body, pd.DataFrame):
+            self.body = TblData(body)
+        else:
+            self.body = body
         self.data = data
 
-    def _render_formats(self) -> "Body":
-        self = Body(self.body, self.data)
-        return self
+    # def _render_formats(self) -> "Body":
+
+    #     self = Body(self.body, self.data)
+    #     formats = self.
+    #     for fmt in formats:
+    #         pass
+    #     # for (fmt in formats)  {
+    #     #     # Determine if the formatter has a function relevant
+    #     #     # to the context; if not, use the `default` function
+    #     #     # (which should always be present)
+    #     #     if (context %in% names(fmt$func)) {
+    #     #     eval_func <- context
+    #     #     } else {
+    #     #     eval_func <- "default"
+    #     #     }
+    #     #     for (col in fmt[["cols"]]) {
+    #     #     # Perform rendering but only do so if the column is present
+    #     #     if (col %in% colnames(data_tbl)) {
+    #     #         result <- fmt$func[[eval_func]](data_tbl[[col]][fmt$rows])
+    #     #         # If any of the resulting output is `NA`, that
+    #     #         # means we want to NOT make changes to those
+    #     #         # particular cells' output (i.e. inherit the
+    #     #         # results of the previous formatter).
+    #     #         body[[col]][fmt$rows][!is.na(result)] <- stats::na.omit(result)
+    #     #     }
+    #     #     }
+    #     # }
+
+    #     return self
 
 
 class BodyAPI:
