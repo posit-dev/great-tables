8eb103cf827497baa78394881dd1615107b44eb0
Richard Iannone <riannone@me.com>
2022-09-27 19:12:08 -0400

    Update _body.py

--- gt/_body.py
+++ gt/_body.py
@@ -1,18 +1,32 @@
-from typing import Optional, Union
+from typing import Union, List, Literal, Any
 import pandas as pd
 from ._tbl_data import TblData
+from ._formats import FormatInfo
+
+Context = Literal["html", "default"]
 
 
 class Body:
     body: TblData
-    data: Optional[int]
+    data: Any
 
-    def __init__(self, body: Union[pd.DataFrame, TblData], data: Optional[int] = None):
+    def __init__(self, body: Union[pd.DataFrame, TblData], data: Any = None):
         if isinstance(body, pd.DataFrame):
             self.body = TblData(body)
         else:
             self.body = body
         self.data = data
+
+    def render_formats(
+        self, data_tbl: TblData, formats: List[FormatInfo], context: Context
+    ):
+        for fmt in formats:
+            eval_func = getattr(fmt.func, context, fmt.func.default)
+            if eval_func is None:
+                raise Exception("Internal Error")
+            for col, row in fmt.cells.resolve():
+                result = eval_func(data_tbl._get_cell(row, col))
+                self.body._set_cell(row, col, result)
 
     # def _render_formats(self) -> "Body":
 
@@ -48,5 +62,5 @@ class Body:
 class BodyAPI:
     _body: Body
 
-    def __init__(self):
-        pass
+    def __init__(self, data: Any):
+        self._body = Body(data, self)
