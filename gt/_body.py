f1a0644963ec4ef8d3850935548caf312f97b265
Richard Iannone <riannone@me.com>
2022-10-06 15:57:52 -0400

    Remove unneeded statements

--- gt/_body.py
+++ gt/_body.py
@@ -30,36 +30,6 @@ class Body:
 
         return self
 
-    # def _render_formats(self) -> "Body":
-
-    #     self = Body(self.body, self.data)
-    #     formats = self.
-    #     for fmt in formats:
-    #         pass
-    #     # for (fmt in formats)  {
-    #     #     # Determine if the formatter has a function relevant
-    #     #     # to the context; if not, use the `default` function
-    #     #     # (which should always be present)
-    #     #     if (context %in% names(fmt$func)) {
-    #     #     eval_func <- context
-    #     #     } else {
-    #     #     eval_func <- "default"
-    #     #     }
-    #     #     for (col in fmt[["cols"]]) {
-    #     #     # Perform rendering but only do so if the column is present
-    #     #     if (col %in% colnames(data_tbl)) {
-    #     #         result <- fmt$func[[eval_func]](data_tbl[[col]][fmt$rows])
-    #     #         # If any of the resulting output is `NA`, that
-    #     #         # means we want to NOT make changes to those
-    #     #         # particular cells' output (i.e. inherit the
-    #     #         # results of the previous formatter).
-    #     #         body[[col]][fmt$rows][!is.na(result)] <- stats::na.omit(result)
-    #     #     }
-    #     #     }
-    #     # }
-
-    #     return self
-
 
 class BodyAPI:
     _body: Body
