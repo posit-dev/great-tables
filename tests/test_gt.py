bf80e1b5779c7998adc646914e6e23965d5b95ca
Richard Iannone <riannone@me.com>
2023-02-10 16:16:05 -0500

    Update test_gt.py

--- tests/test_gt.py
+++ tests/test_gt.py
@@ -6,7 +6,6 @@ data = [{"a": 5, "b": 15}, {"a": 15, "b": 2000}]
 pd_data = pd.DataFrame(data)
 gt_tbl = gt.GT(pd_data)
 
-
 def test_gt_object_prerender():
 
     assert type(gt_tbl).__name__ == "GT"
@@ -28,22 +27,22 @@ def test_gt_object_prerender():
 def test_gt_table_render():
 
     # Assert that a table render process will generate a string object
-    assert type(gt_tbl.render()).__name__ == "str"
+    assert type(gt_tbl.render(context="html")).__name__ == "str"
 
     assert (
         type(
-            gt_tbl.tab_header(title="Title of Table", subtitle="The Subtitle").render()
+            gt_tbl.tab_header(title="Title of Table", subtitle="The Subtitle").render(context="html")
         ).__name__
         == "str"
     )
 
-    assert type(gt_tbl.tab_source_note(source_note="Note").render()).__name__ == "str"
+    assert type(gt_tbl.tab_source_note(source_note="Note").render(context="html")).__name__ == "str"
 
     assert (
         type(
             gt_tbl.tab_source_note(source_note="Note 1")
             .tab_source_note(source_note="Note 2")
-            .render()
+            .render(context="html")
         ).__name__
         == "str"
     )
