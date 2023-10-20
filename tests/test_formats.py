9a5d754a2b3a14ea9eaf925dfffdc71ae4347221
Richard Iannone <riannone@me.com>
2023-10-20 16:42:05 -0400

    Update test_formats.py

--- tests/test_formats.py
+++ tests/test_formats.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import pytest
 from pandas.testing import assert_frame_equal
 
 from gt import GT
 
 
+@pytest.mark.xfail(reason="Don't have a method to get formatted values as list")
 def test_fmt_number_basic():
     df = pd.DataFrame({"x": [1.234, 2.345], "y": [3.456, 4.567]})
     gt = GT(df).fmt_number(columns="x", decimals=2)
