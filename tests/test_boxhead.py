77e85cf2a7ebdd58098e2a6b786806e03eaf7787
Richard Iannone <riannone@me.com>
2023-10-20 16:42:00 -0400

    Create test_boxhead.py

---
+++ tests/test_boxhead.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from gt import GT
+from gt.gt import _get_column_labels
+
+
+def test_cols_label():
+    df = pd.DataFrame({"x": [1.234, 2.345], "y": [3.456, 4.567]})
+    gt = GT(df).cols_label(x="ex", y="why")
+
+    x = _get_column_labels(gt=gt, context="html")
+    y = ["ex", "why"]
+    assert x == y
