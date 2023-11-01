a2bb9f069839a3c343ab39462d6f8a196e5a375c
Richard Iannone <riannone@me.com>
2023-11-01 09:52:10 -0400

    Add explanatory comments and TODOs for debug

--- great_tables/utils_render_common.py
+++ great_tables/utils_render_common.py
@@ -10,11 +10,15 @@ TupleStartFinal = Tuple[int, int]
 
 
 def get_row_reorder_df(groups: RowGroups, stub_df: Stub) -> list[TupleStartFinal]:
-    n_stub_groups = len([entry for entry in stub_df if entry.group_id is not None])
+    # Get the number of non-None entries in the `groupname_col`
+    n_stub_entries = len([entry for entry in stub_df if entry.group_id is not None])
 
-    if n_stub_groups and not len(groups):
-        raise ValueError(f"Detected {n_stub_groups} but only {len(groups)} row groups.")
+    # Raise a ValueError if there are row group entries but no RowGroups
+    if n_stub_entries and not len(groups):
+        raise ValueError(f"Detected {n_stub_entries} but only {len(groups)} row groups.")
 
+    # If there aren't any row groups then return a list of tuples that don't lead
+    # to any resorting later on (e.g., `[(0, 0), (1, 1), (2, 2) ... (n, n)]`)
     if not len(groups):
         indices = range(len(stub_df))
 
@@ -23,9 +27,19 @@ def get_row_reorder_df(groups: RowGroups, stub_df: Stub) -> list[TupleStartFinal
         return [(ii, ii) for ii in indices]
 
     # where in the group each element is
+    # TODO: this doesn't yield consistent values
     groups_pos = [groups.index(row.group_id) if row.group_id is not None else -1 for row in stub_df]
+
+    # From running test_body_reassemble():
+    # print(groups_pos)
+    # [0, 1, 0, 1] <- correct
+    # [1, 0, 1, 0] <- wrong
+
     # the index that when used on the rows will sort them by the order in groups
     start_pos = list(range(len(groups_pos)))
     sort_indx = sorted(start_pos, key=lambda ii: groups_pos[ii])
 
+    # From running test_body_reassemble():
+    # [(0, 0), (1, 2), (2, 1), (3, 3)] <- correct
+    # [(0, 1), (1, 3), (2, 0), (3, 2)] <- wrong
     return list(zip(start_pos, sort_indx))
