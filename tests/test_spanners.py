9cf1e19c63800690888574f85345dc3a6da7ee66
Michael Chow <mc_al_github@fastmail.com>
2023-11-06 13:36:35 -0500

    tests: more spanner tests

--- tests/test_spanners.py
+++ tests/test_spanners.py
@@ -1,32 +1,57 @@
+import pytest
+
 from great_tables._spanners import spanners_print_matrix, empty_spanner_matrix
 from great_tables._gt_data import Spanners, SpannerInfo, Boxhead, ColInfo
 
 
-# TODO: test arguments
-
-
-def test_spanners_print_matrix():
-    spanners = Spanners(
+@pytest.fixture
+def spanners():
+    return Spanners(
         [
             SpannerInfo(spanner_id="a", spanner_level=0, vars=["col1"], built="A"),
             SpannerInfo(spanner_id="b", spanner_level=1, vars=["col2"], built="B"),
         ]
     )
 
-    boxhead = Boxhead(
+
+@pytest.fixture
+def boxhead():
+    return Boxhead(
         [
             ColInfo(var="col1"),
             ColInfo(var="col2"),
             ColInfo(var="col3"),
+            ColInfo(var="col4", visible=False),
         ]
     )
 
+
+def test_spanners_print_matrix(spanners, boxhead):
     mat, vars = spanners_print_matrix(spanners, boxhead)
     assert vars == ["col1", "col2", "col3"]
     assert mat == [
         {"col1": None, "col2": "B", "col3": None},
         {"col1": "A", "col2": None, "col3": None},
         {"col1": "col1", "col2": "col2", "col3": "col3"},
+    ]
+
+
+def test_spanners_print_matrix_arg_omit_columns_row(spanners, boxhead):
+    mat, vars = spanners_print_matrix(spanners, boxhead, omit_columns_row=True)
+    assert vars == ["col1", "col2", "col3"]
+    assert mat == [
+        {"col1": None, "col2": "B", "col3": None},
+        {"col1": "A", "col2": None, "col3": None},
+    ]
+
+
+def test_spanners_print_matrix_arg_include_hidden(spanners, boxhead):
+    mat, vars = spanners_print_matrix(spanners, boxhead, include_hidden=True)
+    assert vars == ["col1", "col2", "col3", "col4"]
+    assert mat == [
+        {"col1": None, "col2": "B", "col3": None, "col4": None},
+        {"col1": "A", "col2": None, "col3": None, "col4": None},
+        {"col1": "col1", "col2": "col2", "col3": "col3", "col4": "col4"},
     ]
 
 
@@ -38,7 +63,7 @@ def test_empty_spanner_matrix():
 
 
 def test_empty_spanner_matrix_arg_omit_columns_row():
-    mat, vars = empty_spanner_matrix(["a", "b"], omit_columns_row=False)
+    mat, vars = empty_spanner_matrix(["a", "b"], omit_columns_row=True)
 
     assert vars == ["a", "b"]
     assert mat == []
