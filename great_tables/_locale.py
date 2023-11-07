ab203bb66e28e791dea6a7e6f3a8614f772aec08
Richard Iannone <riannone@me.com>
2023-11-07 12:54:09 -0500

    Add `_get_locales_data()` helper fn

--- great_tables/_locale.py
+++ great_tables/_locale.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 from typing import Optional
+import pandas as pd
+import pkg_resources
 
 
 class Locale:
@@ -13,3 +15,63 @@ class Locale:
 
 class LocaleAPI:
     pass
+
+
+def _get_locales_data() -> pd.DataFrame:
+    _x_locales_fname = pkg_resources.resource_filename("great_tables.data", "x_locales.csv")
+    _x_locales_dtype = {
+        "country_name": "object",
+        "country_code_2": "object",
+        "country_code_3": "object",
+        "year": "Int64",
+        "population": "Int64",
+        "locale": "object",
+        "lang_name": "object",
+        "lang_desc": "object",
+        "script_name": "object",
+        "script_desc": "object",
+        "territory_name": "object",
+        "territory_desc": "object",
+        "variant_name": "object",
+        "variant_desc": "object",
+        "chr_index": "object",
+        "decimal": "object",
+        "group": "object",
+        "percent_sign": "object",
+        "plus_sign": "object",
+        "minus_sign": "object",
+        "approx_sign": "object",
+        "exp_sign": "object",
+        "sup_exp": "object",
+        "per_mille": "object",
+        "infinity": "object",
+        "nan": "object",
+        "approx_pattern": "object",
+        "at_least_pattern": "object",
+        "at_most_pattern": "object",
+        "range_pattern": "object",
+        "decimal_format": "object",
+        "sci_format": "object",
+        "percent_format": "object",
+        "currency_format": "object",
+        "accounting_format": "object",
+        "default_numbering_system": "object",
+        "minimum_grouping_digits": "Int64",
+        "currency_code": "object",
+        "no_table_data_text": "object",
+        "sort_label_text": "object",
+        "filter_label_text": "object",
+        "search_placeholder_text": "object",
+        "page_next_text": "object",
+        "page_previous_text": "object",
+        "page_numbers_text": "object",
+        "page_info_text": "object",
+        "page_size_options_text": "object",
+        "page_next_label_text": "object",
+        "page_previous_label_text": "object",
+        "page_number_label_text": "object",
+        "page_jump_label_text": "object",
+        "page_size_options_label_text": "object",
+    }
+    __x_locales: pd.DataFrame = pd.read_csv(_x_locales_fname, dtype=_x_locales_dtype)
+    return __x_locales
