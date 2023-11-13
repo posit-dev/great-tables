539f9076844c7279c47d485067fa4979df5dbdcb
Richard Iannone <riannone@me.com>
2023-11-13 15:26:48 -0500

    Add the `cols_align()` method to BoxheadAPI

--- great_tables/_boxhead.py
+++ great_tables/_boxhead.py
@@ -7,7 +7,7 @@ from ._tbl_data import get_column_names
 from ._utils import _assert_list_is_subset
 
 
-class BoxheadAPI():
+class BoxheadAPI:
     def cols_label(self, **kwargs: str):
         """
         Relabel one or more columns.
@@ -53,6 +53,38 @@ class BoxheadAPI():
 
         for i in range(len(kwargs)):
             self._boxhead._set_column_label(column=mod_columns[i], label=new_labels[i])
+
+        return self
+
+    def cols_align(self, align: str = "left", columns: str = None):
+        """
+        Set the alignment of one or more columns.
+
+        The `cols_align()` method sets the alignment of one or more columns.
+        The `align` argument can be set to one of `"left"`, `"center"`, or
+        `"right"` and the `columns` argument can be used to specify which
+        columns to apply the alignment to. If `columns` is not specified, the
+        alignment is applied to all columns.
+
+        Returns
+        -------
+        GT
+            Result of the table operation.
+        """
+
+        # Get the full list of column names for the data
+        column_names = self._boxhead._get_columns()
+
+        # Stop function if any of the column names specified are not in `cols_labels`
+        # msg: "All column names provided must exist in the input `.data` table."
+        if columns is not None:
+            _assert_list_is_subset(columns, column_names)
+
+        if columns is None:
+            columns = column_names
+
+        for column in columns:
+            self._boxhead._set_column_align(column=column, align=align)
 
         return self
 
