3fec919b2b2732b1dc6fff66e4de9d8d1259749b
Richard Iannone <riannone@me.com>
2023-11-20 15:23:16 -0500

    Improve docs for helper functions

--- great_tables/_helpers.py
+++ great_tables/_helpers.py
@@ -8,18 +8,16 @@ def px(x: Union[int, float]) -> str:
     """
     Helper for providing a CSS length value in pixels.
 
-    For certain parameters, a length value is required. Examples include the
-    setting of font sizes (e.g., in `cell_text()`) and thicknesses of lines
-    (e.g., in `cell_borders()`). Setting a length in pixels with `px()` allows
-    for an absolute definition of size as opposed to the analogous helper
-    function `pct()`.
+    For certain parameters, a length value is required. Examples include the setting of font sizes
+    (e.g., in `cell_text()`) and thicknesses of lines (e.g., in `cell_borders()`). Setting a length
+    in pixels with `px()` allows for an absolute definition of size as opposed to the analogous
+    helper function `pct()`.
 
     Parameters
     ----------
-    x (Union[int, float])
-        The integer or float value to format as a string (e.g., `"12px"`) for
-        some `tab_options()` arguments that can take values as units of
-        pixels (e.g., `table_font_size`).
+    x : Union[int, float]
+        The integer or float value to format as a string (e.g., `"12px"`) for some arguments that
+        can take values as units of pixels.
 
     Examples
     --------
@@ -35,21 +33,18 @@ def pct(x: Union[int, float]) -> str:
     """
     Helper for providing a CSS length value as a percentage.
 
-    A percentage value acts as a length value that is relative to an initial
-    state. For instance an 80% value for something will size the target
-    to 80% the size of its 'previous' value. This type of sizing is
-    useful for sizing up or down a length value with an intuitive measure. This
-    helper function can be used for the setting of font sizes (e.g., in
-    `cell_text()`) and altering the thicknesses of lines (e.g., in
-    `cell_borders()`. Should a more exact definition of size be required, the
-    analogous helper function `pct()` will be more useful.
+    A percentage value acts as a length value that is relative to an initial state. For instance an
+    80% value for something will size the target to 80% the size of its 'previous' value. This type
+    of sizing is useful for sizing up or down a length value with an intuitive measure. This helper
+    function can be used for the setting of font sizes (e.g., in `cell_text()`) and altering the
+    thicknesses of lines (e.g., in `cell_borders()`. Should a more exact definition of size be
+    required, the analogous helper function `pct()` will be more useful.
 
     Parameters
     ----------
-    x (Union[int, float])
-        The integer or float value to format as a string-based percentage value
-        for some `tab_options()` arguments that can take percentage values
-        (e.g., `table.width`).
+    x : Union[int, float]
+        The integer or float value to format as a string-based percentage value for some arguments
+        that can take percentage values.
 
     Examples
     --------
@@ -64,19 +59,20 @@ def pct(x: Union[int, float]) -> str:
 def md(text: str) -> Text:
     """Interpret input text as Markdown-formatted text.
 
-    Markdown! It's a wonderful thing. We can use it in certain places (e.g.,
-    footnotes, source notes, the table title, etc.) and expect it to render to
-    HTML as Markdown does. There is the `html()` helper that allows you to ferry
-    in HTML but this function `md()`... it's almost like a two-for-one deal (you
-    get to use Markdown plus any HTML fragments at the same time).
+    Markdown! It's a wonderful thing. We can use it in certain places (e.g., footnotes, source
+    notes, the table title, etc.) and expect it to render to HTML as Markdown does. There is the
+    `html()` helper that allows you to ferry in HTML but this function `md()`... it's almost like a
+    two-for-one deal (you get to use Markdown plus any HTML fragments at the same time).
 
-    Args:
-        text (str): The text that is understood to contain Markdown
-        formatting.
+    Parameters
+    ----------
+    text : str
+        The text that is understood to contain Markdown formatting.
 
-    Returns:
-        Text: An instance of the Text class is returned, where the text
-        `type` is 'markdown'.
+    Returns
+    -------
+    Text
+        An instance of the Text class is returned, where the text `type` is `"from_markdown"`.
     """
     return Text(text=text, type="from_markdown")
 
@@ -84,18 +80,20 @@ def md(text: str) -> Text:
 def html(text: str) -> Text:
     """Interpret input text as HTML-formatted text.
 
-    For certain pieces of text (like in column labels or table headings) we may
-    want to express them as raw HTML. In fact, with HTML, anything goes so it
-    can be much more than just text. The `html()` function will guard the input
-    HTML against escaping, so, your HTML tags will come through as HTML when
-    rendered... to HTML.
+    For certain pieces of text (like in column labels or table headings) we may want to express them
+    as raw HTML. In fact, with HTML, anything goes so it can be much more than just text. The
+    `html()` function will guard the input HTML against escaping, so, your HTML tags will come
+    through as HTML when rendered... to HTML.
 
-    Args:
-        text (str): The text that is understood to contain HTML formatting.
+    Parameters
+    ----------
+    text : str
+        The text that is understood to contain HTML formatting.
 
-    Returns:
-        Text: An instance of the Text class is returned, where the text
-        `type` is 'html'.
+    Returns
+    -------
+    Text
+        An instance of the Text class is returned, where the text `type` is `"html"`.
     """
     return Text(text=text, type="html")
 
@@ -103,12 +101,15 @@ def html(text: str) -> Text:
 def random_id(n: int = 10) -> str:
     """Helper for creating a random `id` for a gt table
 
-    Args:
-        n (int, optional): The number of lowercase letters to use in the random
-        ID string. Defaults to 10.
+    Parameters
+    ----------
+    n : int
+        The number of lowercase letters to use in the random ID string. Defaults to 10.
 
-    Returns:
-        str: A string that constitutes a random ID value.
+    Returns
+    -------
+    str
+        A string that constitutes a random ID value.
     """
     return "".join(random.choices(letters(), k=n))
 
