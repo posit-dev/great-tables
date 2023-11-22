9e95758e569b683fa6641a08602d98bf7cba424a
Richard Iannone <riannone@me.com>
2023-11-22 13:25:13 -0500

    Add the `vals_fmt_bytes()` fn

--- great_tables/_formats_vals.py
+++ great_tables/_formats_vals.py
@@ -575,3 +575,116 @@ def vals_fmt_currency(
     vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
 
     return vals_fmt
+
+
+def vals_fmt_bytes(
+    vals: Union[Any, List[Any]],
+    standard: str = "decimal",
+    decimals: int = 1,
+    n_sigfig: Optional[int] = None,
+    drop_trailing_zeros: bool = True,
+    drop_trailing_dec_mark: bool = True,
+    use_seps: bool = True,
+    pattern: str = "{x}",
+    sep_mark: str = ",",
+    dec_mark: str = ".",
+    force_sign: bool = False,
+    incl_space: bool = True,
+    locale: Union[str, None] = None,
+) -> List[str]:
+    """
+    Format values as bytes.
+
+    With numeric values in a list, we can transform those to values of bytes with human readable
+    units. The `fmt_bytes()` method allows for the formatting of byte sizes to either of two common
+    representations: (1) with decimal units (powers of 1000, examples being `"kB"` and `"MB"`), and
+    (2) with binary units (powers of 1024, examples being `"KiB"` and `"MiB"`). It is assumed the
+    input numeric values represent the number of bytes and automatic truncation of values will
+    occur. The numeric values will be scaled to be in the range of 1 to <1000 and then decorated
+    with the correct unit symbol according to the standard chosen. For more control over the
+    formatting of byte sizes, we can use the following options:
+
+    - decimals: choice of the number of decimal places, option to drop trailing zeros, and a choice
+    of the decimal symbol
+    - digit grouping separators: options to enable/disable digit separators and provide a choice of
+    separator symbol
+    - pattern: option to use a text pattern for decoration of the formatted values
+    - locale-based formatting: providing a locale ID will result in number formatting specific to
+    the chosen locale
+
+    Parameters
+    ----------
+    vals : Union[Any, List[Any]]
+        A list of values to be formatted.
+    standard: str
+        The form of expressing large byte sizes is divided between: (1) decimal units (powers of
+        1000; e.g., `"kB"` and `"MB"`), and (2) binary units (powers of 1024; e.g., `"KiB"` and
+        `"MiB"`). The default is to use decimal units with the `"decimal"` option. The alternative
+        is to use binary units with the `"binary"` option.
+    decimals : int
+        This corresponds to the exact number of decimal places to use. A value such as `2.34` can,
+        for example, be formatted with `0` decimal places and it would result in `"2"`. With `4`
+        decimal places, the formatted value becomes `"2.3400"`. The trailing zeros can be removed
+        with `drop_trailing_zeros=True`.
+    drop_trailing_zeros : bool
+        A boolean value that allows for removal of trailing zeros (those redundant zeros after the
+        decimal mark).
+    drop_trailing_dec_mark : bool
+        A boolean value that determines whether decimal marks should always appear even if there are
+        no decimal digits to display after formatting (e.g., `23` becomes `23.` if `False`). By
+        default trailing decimal marks are not shown.
+    use_seps : bool
+        The `use_seps` option allows for the use of digit group separators. The type of digit group
+        separator is set by `sep_mark` and overridden if a locale ID is provided to `locale`. This
+        setting is `True` by default.
+    pattern : str
+        A formatting pattern that allows for decoration of the formatted value. The formatted value
+        is represented by the `{x}` (which can be used multiple times, if needed) and all other
+        characters will be interpreted as string literals.
+    sep_mark : str
+        The string to use as a separator between groups of digits. For example, using `sep_mark=","`
+        with a value of `1000` would result in a formatted value of `"1,000"`. This argument is
+        ignored if a `locale` is supplied (i.e., is not `None`).
+    dec_mark : str
+        The string to be used as the decimal mark. For example, using `dec_mark=","` with the value
+        `0.152` would result in a formatted value of `"0,152"`). This argument is ignored if a
+        `locale` is supplied (i.e., is not `None`).
+    force_sign : bool
+        Should the positive sign be shown for positive values (effectively showing a sign for all
+        values except zero)? If so, use `True` for this option. The default is `False`, where only
+        negative numbers will display a minus sign. This option is disregarded when using accounting
+        notation with `accounting = True`.
+    incl_space : bool
+        An option for whether to include a space between the value and the currency symbol. The
+        default is to not introduce a space character.
+    locale : str
+        An optional locale identifier that can be used for formatting values according the locale's
+        rules. Examples include `"en"` for English (United States) and `"fr"` for French (France).
+
+    Returns
+    -------
+    List[str]
+        A list of formatted values is returned.
+    """
+
+    gt_obj: GTData = _make_one_col_table(vals=vals)
+
+    gt_obj_fmt = gt_obj.fmt_bytes(
+        columns="x",
+        standard=standard,
+        decimals=decimals,
+        n_sigfig=n_sigfig,
+        drop_trailing_zeros=drop_trailing_zeros,
+        drop_trailing_dec_mark=drop_trailing_dec_mark,
+        use_seps=use_seps,
+        pattern=pattern,
+        sep_mark=sep_mark,
+        dec_mark=dec_mark,
+        force_sign=force_sign,
+        incl_space=incl_space,
+        locale=locale,
+    )
+
+    vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
+
+    return vals_fmt
