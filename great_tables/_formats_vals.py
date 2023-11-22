72308b4de582b361c587231d5d34a17f48bc7505
Richard Iannone <riannone@me.com>
2023-11-22 10:43:32 -0500

    Add the `vals_fmt_scientific()` fn

--- great_tables/_formats_vals.py
+++ great_tables/_formats_vals.py
@@ -212,3 +212,126 @@ def vals_fmt_integer(
     vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
 
     return vals_fmt
+
+
+def vals_fmt_scientific(
+    vals: Union[Any, List[Any]],
+    decimals: int = 2,
+    n_sigfig: Optional[int] = None,
+    drop_trailing_zeros: bool = False,
+    drop_trailing_dec_mark: bool = True,
+    scale_by: float = 1,
+    exp_style: str = "x10n",
+    pattern: str = "{x}",
+    sep_mark: str = ",",
+    dec_mark: str = ".",
+    force_sign_m: bool = False,
+    force_sign_n: bool = False,
+    locale: Union[str, None] = None,
+) -> List[str]:
+    """
+    Format values to scientific notation.
+
+    With numeric values in a list, we can perform formatting so that the input values are rendered
+    in scientific notation, where extremely large or very small numbers can be expressed in a more
+    practical fashion. Here, numbers are written in the form of a mantissa (`m`) and an exponent
+    (`n`) with the construction *m* x 10^*n* or *m*E*n*. The mantissa component is a number between
+    `1` and `10`. For instance, `2.5 x 10^9` can be used to represent the value 2,500,000,000 in
+    scientific notation. In a similar way, 0.00000012 can be expressed as `1.2 x 10^-7`. Due to its
+    ability to describe numbers more succinctly and its ease of calculation, scientific notation is
+    widely employed in scientific and technical domains.
+
+    We have fine control over the formatting task, with the following options:
+
+    - decimals: choice of the number of decimal places, option to drop trailing zeros, and a choice
+    of the decimal symbol
+    - scaling: we can choose to scale targeted values by a multiplier value
+    - pattern: option to use a text pattern for decoration of the formatted values
+    - locale-based formatting: providing a locale ID will result in formatting specific to the
+    chosen locale
+
+    Parameters
+    ----------
+    vals : Union[Any, List[Any]]
+        A list of values to be formatted.
+    decimals : int
+        The `decimals` values corresponds to the exact number of decimal places to use. A value such
+        as `2.34` can, for example, be formatted with `0` decimal places and it would result in
+        `"2"`. With `4` decimal places, the formatted value becomes `"2.3400"`. The trailing zeros
+        can be removed with `drop_trailing_zeros=True`. If you always need `decimals = 0`, the
+        `fmt_integer()` method should be considered.
+    n_sigfig : Optional[int]
+        A option to format numbers to *n* significant figures. By default, this is `None` and thus
+        number values will be formatted according to the number of decimal places set via
+        `decimals`. If opting to format according to the rules of significant figures, `n_sigfig`
+        must be a number greater than or equal to `1`. Any values passed to the `decimals` and
+        `drop_trailing_zeros` arguments will be ignored.
+    drop_trailing_zeros : bool
+        A boolean value that allows for removal of trailing zeros (those redundant zeros after the
+        decimal mark).
+    drop_trailing_dec_mark : bool
+        A boolean value that determines whether decimal marks should always appear even if there are
+        no decimal digits to display after formatting (e.g., `23` becomes `23.` if `False`). By
+        default trailing decimal marks are not shown.
+    scale_by : float
+        All numeric values will be multiplied by the `scale_by` value before undergoing formatting.
+        Since the `default` value is `1`, no values will be changed unless a different multiplier
+        value is supplied.
+    exp_style : str
+        Style of formatting to use for the scientific notation formatting. By default this is
+        `"x10n"` but other options include using a single letter (e.g., `"e"`, `"E"`, etc.), a
+        letter followed by a `"1"` to signal a minimum digit width of one, or `"low-ten"` for using
+        a stylized `"10"` marker.
+    pattern : str
+        A formatting pattern that allows for decoration of the formatted value. The formatted value
+        is represented by the `{x}` (which can be used multiple times, if needed) and all other
+        characters will be interpreted as string literals.
+    sep_mark : str
+        The string to use as a separator between groups of digits. For example, using `sep_mark=","`
+        with a value of `1000` would result in a formatted value of `"1,000"`. This argument is
+        ignored if a `locale` is supplied (i.e., is not `None`).
+    dec_mark : str
+        The string to be used as the decimal mark. For example, using `dec_mark=","` with the value
+        `0.152` would result in a formatted value of `"0,152"`). This argument is ignored if a
+        `locale` is supplied (i.e., is not `None`).
+    force_sign_m : bool
+        Should the plus sign be shown for positive values of the mantissa (first component)? This
+        would effectively show a sign for all values except zero on the first numeric component of
+        the notation. If so, use `True` (the default for this is `False`), where only negative
+        numbers will display a sign.
+    force_sign_n : bool
+        Should the plus sign be shown for positive values of the exponent (second component)? This
+        would effectively show a sign for all values except zero on the second numeric component of
+        the notation. If so, use `True` (the default for this is `False`), where only negative
+        numbers will display a sign.
+    locale : str
+        An optional locale identifier that can be used for formatting values according the locale's
+        rules. Examples include `"en"` for English (United States) and `"fr"` for French (France).
+
+    Returns
+    -------
+    List[str]
+        A list of formatted values is returned.
+    """
+
+    gt_obj: GTData = _make_one_col_table(vals=vals)
+
+    gt_obj_fmt = gt_obj.fmt_scientific(
+        columns="x",
+        decimals=decimals,
+        n_sigfig=n_sigfig,
+        drop_trailing_zeros=drop_trailing_zeros,
+        drop_trailing_dec_mark=drop_trailing_dec_mark,
+        scale_by=scale_by,
+        exp_style=exp_style,
+        pattern=pattern,
+        sep_mark=sep_mark,
+        dec_mark=dec_mark,
+        force_sign_m=force_sign_m,
+        force_sign_n=force_sign_n,
+        locale=locale,
+    )
+
+    vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
+
+    return vals_fmt
