24fe9652306c4194b72a5dd4e0c9638cafac468c
Richard Iannone <riannone@me.com>
2023-11-22 10:48:55 -0500

    Add the `vals_fmt_percent()` fn

--- great_tables/_formats_vals.py
+++ great_tables/_formats_vals.py
@@ -335,3 +335,121 @@ def vals_fmt_scientific(
     vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
 
     return vals_fmt
+
+
+def vals_fmt_percent(
+    vals: Union[Any, List[Any]],
+    decimals: int = 2,
+    drop_trailing_zeros: bool = False,
+    drop_trailing_dec_mark: bool = True,
+    scale_values: bool = True,
+    use_seps: bool = True,
+    pattern: str = "{x}",
+    sep_mark: str = ",",
+    dec_mark: str = ".",
+    force_sign: bool = False,
+    placement: str = "right",
+    incl_space: bool = False,
+    locale: Union[str, None] = None,
+) -> List[str]:
+    """
+    Format values as a percentage.
+
+    With numeric values in a list, we can perform percentage-based formatting. It is assumed the
+    input numeric values are proportional values and, in this case, the values will be automatically
+    multiplied by `100` before decorating with a percent sign (the other case is accommodated though
+    setting `scale_values` to `False`). For more control over percentage formatting, we can use the
+    following options:
+
+    - percent sign placement: the percent sign can be placed after or before the values and a space
+    can be inserted between the symbol and the value.
+    - decimals: choice of the number of decimal places, option to drop trailing zeros, and a choice
+    of the decimal symbol
+    - digit grouping separators: options to enable/disable digit separators and provide a choice of
+    separator symbol
+    - value scaling toggle: choose to disable automatic value scaling in the situation that values
+    are already scaled coming in (and just require the percent symbol)
+    - pattern: option to use a text pattern for decoration of the formatted values
+    - locale-based formatting: providing a locale ID will result in number formatting specific to
+    the chosen locale
+
+    Parameters
+    ----------
+    vals : Union[Any, List[Any]]
+        A list of values to be formatted.
+    decimals : int
+        The `decimals` values corresponds to the exact number of decimal places to use. A value such
+        as `2.34` can, for example, be formatted with `0` decimal places and it would result in
+        `"2"`. With `4` decimal places, the formatted value becomes `"2.3400"`. The trailing zeros
+        can be removed with `drop_trailing_zeros=True`. If you always need `decimals = 0`, the
+        `fmt_integer()` method should be considered.
+    drop_trailing_zeros : bool
+        A boolean value that allows for removal of trailing zeros (those redundant zeros after the
+        decimal mark).
+    drop_trailing_dec_mark : bool
+        A boolean value that determines whether decimal marks should always appear even if there are
+        no decimal digits to display after formatting (e.g., `23` becomes `23.` if `False`). By
+        default trailing decimal marks are not shown.
+    scale_values : bool
+        Should the values be scaled through multiplication by 100? By default this scaling is
+        performed since the expectation is that incoming values are usually proportional. Setting to
+        `False` signifies that the values are already scaled and require only the percent sign when
+        formatted.
+    use_seps : bool
+        The `use_seps` option allows for the use of digit group separators. The type of digit group
+        separator is set by `sep_mark` and overridden if a locale ID is provided to `locale`. This
+        setting is `True` by default.
+    pattern : str
+        A formatting pattern that allows for decoration of the formatted value. The formatted value
+        is represented by the `{x}` (which can be used multiple times, if needed) and all other
+        characters will be interpreted as string literals.
+    sep_mark : str
+        The string to use as a separator between groups of digits. For example, using `sep_mark=","`
+        with a value of `1000` would result in a formatted value of `"1,000"`. This argument is
+        ignored if a `locale` is supplied (i.e., is not `None`).
+    dec_mark : str
+        The string to be used as the decimal mark. For example, using `dec_mark=","` with the value
+        `0.152` would result in a formatted value of `"0,152"`). This argument is ignored if a
+        `locale` is supplied (i.e., is not `None`).
+    force_sign : bool
+        Should the positive sign be shown for positive values (effectively showing a sign for all
+        values except zero)? If so, use `True` for this option. The default is `False`, where only
+        negative numbers will display a minus sign. This option is disregarded when using accounting
+        notation with `accounting = True`.
+    placement : str
+        This option governs the placement of the percent sign. This can be either be `"right"` (the
+        default) or `"left"`.
+    incl_space : bool
+        An option for whether to include a space between the value and the percent sign. The default
+        is to not introduce a space character.
+    locale : str
+        An optional locale identifier that can be used for formatting values according the locale's
+        rules. Examples include `"en"` for English (United States) and `"fr"` for French (France).
+
+    Returns
+    -------
+    List[str]
+        A list of formatted values is returned.
+    """
+
+    gt_obj: GTData = _make_one_col_table(vals=vals)
+
+    gt_obj_fmt = gt_obj.fmt_percent(
+        columns="x",
+        decimals=decimals,
+        drop_trailing_zeros=drop_trailing_zeros,
+        drop_trailing_dec_mark=drop_trailing_dec_mark,
+        scale_values=scale_values,
+        use_seps=use_seps,
+        pattern=pattern,
+        sep_mark=sep_mark,
+        dec_mark=dec_mark,
+        force_sign=force_sign,
+        placement=placement,
+        incl_space=incl_space,
+        locale=locale,
+    )
+
+    vals_fmt = _get_column_of_values(gt=gt_obj_fmt, column_name="x", context="html")
+
+    return vals_fmt
