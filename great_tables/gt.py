a6a6b09f68db8f46c50233e118123ccc5af0445a
Richard Iannone <riannone@me.com>
2023-11-27 11:13:02 -0500

    Try to force new paragraph with linebreak

--- great_tables/gt.py
+++ great_tables/gt.py
@@ -140,6 +140,7 @@ class GT(
     This sets up a table with a stub, the row labels are placed within the stub column, and a
     vertical dividing line has been placed on the right-hand side.
 
+
     By default, values in the body of a table (and their column labels) are automatically aligned.
     The alignment is governed by the types of values in a column. If you'd like to disable this form
     of auto-alignment, the `auto_align=False` option can be taken.
