85644f8f7e8aa258c6effdf3c9433101c998bf55
Richard Iannone <riannone@me.com>
2023-12-01 14:41:00 -0500

    Create vals module

---
+++ great_tables/vals.py
@@ -0,0 +1,12 @@
+from ._formats_vals import (
+    val_fmt_number as fmt_number,
+    val_fmt_integer as fmt_integer,
+    val_fmt_scientific as fmt_scientific,
+    val_fmt_percent as fmt_percent,
+    val_fmt_currency as fmt_currency,
+    val_fmt_roman as fmt_roman,
+    val_fmt_bytes as fmt_bytes,
+    val_fmt_date as fmt_date,
+    val_fmt_time as fmt_time,
+    val_fmt_markdown as fmt_markdown,
+)
