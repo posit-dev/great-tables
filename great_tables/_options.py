f3f652c8cd67a78be6021ec062b58d59bc94c715
Richard Iannone <riannone@me.com>
2024-02-01 13:27:00 -0500

    Add the `opt_vertical_padding()` method

--- great_tables/_options.py
+++ great_tables/_options.py
@@ -736,16 +736,16 @@ def opt_align_table_header(self: GTSelf, align: str = "center") -> GTSelf:
     (
       GT(
         exibble[["num", "char", "currency", "row", "group"]],
-        rowname_col = "row",
-        groupname_col = "group"
+        rowname_col="row",
+        groupname_col="group"
       )
       .tab_header(
-        title = md("Data listing from **exibble**"),
-        subtitle = md("`exibble` is a **Great Tables** dataset.")
+        title=md("Data listing from **exibble**"),
+        subtitle=md("`exibble` is a **Great Tables** dataset.")
       )
-      .fmt_number(columns = "num")
-      .fmt_currency(columns = "currency")
-      .tab_source_note(source_note = "This is only a subset of the dataset.")
+      .fmt_number(columns="num")
+      .fmt_currency(columns="currency")
+      .tab_source_note(source_note="This is only a subset of the dataset.")
       .opt_align_table_header(align="left")
     )
     ```
@@ -756,9 +756,102 @@ def opt_align_table_header(self: GTSelf, align: str = "center") -> GTSelf:
     return tab_options(self, heading_align=align)
 
 
-# TODO: create the `opt_vertical_padding()` method
+def opt_vertical_padding(self: GTSelf, scale: float = 1.0) -> GTSelf:
+    """
+    Option to scale the vertical padding of the table.
 
-# TODO: create the `opt_horizontal_padding()` method
+    This method allows us to scale the vertical padding of the table by a factor of `scale`. The
+    default value is `1.0` and this method serves as a convenient shortcut for
+    `gt.tab_options(heading_padding=<new_val>, column_labels_padding=<new_val>,
+    data_row_padding=<new_val>, row_group_padding=<new_val>, summary_row_padding=<new_val>,
+    grand_summary_row_padding=<new_val>, footnotes_padding=<new_val>,
+    source_notes_padding=<new_val>)`.
+
+    Parameters
+    ----------
+    scale : float
+        The factor by which to scale the vertical padding. The default value is `1.0`. A value
+        less than `1.0` will reduce the padding, and a value greater than `1.0` will increase the
+        padding. The value must be between `0` and `3`.
+
+    Returns
+    -------
+    GT
+        The GT object is returned. This is the same object that the method is called on so that we
+        can facilitate method chaining.
+
+    Examples
+    --------
+    Using select columns from the `exibble` dataset, let's create a table with a number of
+    components added. Following that, we'll scale the vertical padding of the table by a factor of
+    `1.5` using the `opt_vertical_padding()` method.
+
+    ```{python}
+    from great_tables import GT, exibble, md
+
+    gt_tbl = (
+        GT(
+            exibble[["num", "char", "currency", "row", "group"]],
+            rowname_col="row",
+            groupname_col="group"
+        )
+        .tab_header(
+            title=md("Data listing from **exibble**"),
+            subtitle=md("`exibble` is a **Great Tables** dataset.")
+        )
+        .fmt_number(columns = "num")
+        .fmt_currency(columns = "currency")
+        .tab_source_note(source_note = "This is only a subset of the dataset.")
+    )
+
+    gt_tbl.opt_vertical_padding(scale=1.5)
+    ```
+
+    Let's go the other way and scale the vertical padding of the table by a factor of `0.5` using
+    the `opt_vertical_padding()` method.
+
+    ```{python}
+    gt_tbl.opt_vertical_padding(scale=0.5)
+    ```
+    """
+
+    # Stop if `scale` is beyond an acceptable range
+    if scale < 0 or scale > 3:
+        raise ValueError("`scale` must be a value between `0` and `3`.")
+
+    # Get the parameters from the options that relate to vertical padding
+    vertical_padding_params = [
+        "heading_padding",
+        "column_labels_padding",
+        "data_row_padding",
+        "row_group_padding",
+        "summary_row_padding",
+        "grand_summary_row_padding",
+        "footnotes_padding",
+        "source_notes_padding",
+    ]
+
+    # Get the current values for the vertical padding parameters
+    vertical_padding_vals = [
+        self._options.heading_padding.value,
+        self._options.column_labels_padding.value,
+        self._options.data_row_padding.value,
+        self._options.row_group_padding.value,
+        self._options.summary_row_padding.value,
+        self._options.grand_summary_row_padding.value,
+        self._options.footnotes_padding.value,
+        self._options.source_notes_padding.value,
+    ]
+
+    # Multiply each of the padding values by the `scale` factor but strip off the units first
+    # then reattach the units after the multiplication
+    # TODO: a current limitation is that the padding values must be in pixels and not percentages
+    # TODO: another limitation is that the returned values must be in integer pixel values
+    new_vertical_padding_vals = [
+        str(int(float(v.split("px")[0]) * scale)) + "px" for v in vertical_padding_vals
+    ]
+
+    return tab_options(self, **dict(zip(vertical_padding_params, new_vertical_padding_vals)))
 
 
 def opt_all_caps(
