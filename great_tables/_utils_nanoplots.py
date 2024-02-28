e2e2b96d20e7946c484aec9002e1ed7b89e18033
Richard Iannone <riannone@me.com>
2024-02-28 00:45:32 -0500

    Incorporate `ref_line_tags` into SVG plot

--- great_tables/_utils_nanoplots.py
+++ great_tables/_utils_nanoplots.py
@@ -261,11 +261,12 @@ def _construct_nanoplot_svg(
     svg_style: str,
     area_path_tags: str,
     data_path_tags: str,
+    ref_line_tags: str,
     circle_tags: str,
 ) -> str:
 
     # FIXME: remove the style attribute on the surrounding div
-    return f'<div style="width:500px;height:200px"><svg role="img" "viewBox="{viewbox} "style=height:{svg_height};margin-left:auto;margin-right:auto;font-size:inherit;overflow:visible;vertical-align:middle;position:relative;">{svg_defs}{svg_style}{area_path_tags}{data_path_tags}{circle_tags}</svg></div>'
+    return f'<div style="width:500px;height:200px"><svg role="img" "viewBox="{viewbox} "style=height:{svg_height};margin-left:auto;margin-right:auto;font-size:inherit;overflow:visible;vertical-align:middle;position:relative;">{svg_defs}{svg_style}{area_path_tags}{data_path_tags}{ref_line_tags}{circle_tags}</svg></div>'
 
 
 def _generate_nanoplot(
@@ -941,6 +942,7 @@ def _generate_nanoplot(
         svg_style=svg_style,
         area_path_tags=area_path_tags,
         data_path_tags=data_path_tags,
+        ref_line_tags=ref_line_tags,
         circle_tags=circle_tags,
     )
 
