1c3f90f6db62648f5c009c71042c2559c9e9be1b
Richard Iannone <riannone@me.com>
2024-03-11 00:08:55 -0400

    Simplify `_units_symbol_replacements()`

--- great_tables/_utils_units_notation.py
+++ great_tables/_utils_units_notation.py
@@ -163,76 +163,102 @@ def _replace_units_symbol(text: str, detect: str, pattern: str, replace: str) ->
 
 def _units_symbol_replacements(text: str) -> str:
 
+    # Replace certain units symbols with HTML entities; these are cases where the parsed
+    # text should be at the beginning of a string (or should be the entire string)
     text = _replace_units_symbol(text, "^-", "^-", "&minus;")
     text = _replace_units_symbol(text, "^um$", "um", "&micro;m")
     text = _replace_units_symbol(text, "^uL$", "uL", "&micro;L")
     text = _replace_units_symbol(text, "^umol", "^umol", "&micro;mol")
     text = _replace_units_symbol(text, "^ug$", "ug", "&micro;g")
-    text = _replace_units_symbol(text, ":micro:", ":micro:", "&micro;")
-    text = _replace_units_symbol(text, ":mu:", ":mu:", "&micro;")
     text = _replace_units_symbol(text, "^ohm$", "ohm", "&#8486;")
-    text = _replace_units_symbol(text, ":ohm:", ":ohm:", "&#8486;")
-    text = _replace_units_symbol(text, ":angstrom:", ":angstrom:", "&#8491;")
-    text = _replace_units_symbol(text, ":times:", ":times:", "&times;")
-    text = _replace_units_symbol(text, ":plusminus:", ":plusminus:", "&plusmn;")
-    text = _replace_units_symbol(text, ":permil:", ":permil:", "&permil;")
-    text = _replace_units_symbol(text, ":permille:", ":permille:", "&permil;")
-    text = _replace_units_symbol(text, ":degree:", ":degree:", "&deg;")
-    text = _replace_units_symbol(text, ":degrees:", ":degrees:", "&deg;")
-    text = _replace_units_symbol(text, "degC", "degC", "&deg;C")
-    text = _replace_units_symbol(text, "degF", "degF", "&deg;F")
-    text = _replace_units_symbol(text, ":space:", ":space:", "&nbsp;")
-    text = _replace_units_symbol(text, ":Alpha:", ":Alpha:", "&Alpha;")
-    text = _replace_units_symbol(text, ":alpha:", ":alpha:", "&alpha;")
-    text = _replace_units_symbol(text, ":Beta:", ":Beta:", "&Beta;")
-    text = _replace_units_symbol(text, ":beta:", ":beta:", "&beta;")
-    text = _replace_units_symbol(text, ":Gamma:", ":Gamma:", "&Gamma;")
-    text = _replace_units_symbol(text, ":gamma:", ":gamma:", "&gamma;")
-    text = _replace_units_symbol(text, ":Delta:", ":Delta:", "&Delta;")
-    text = _replace_units_symbol(text, ":delta:", ":delta:", "&delta;")
-    text = _replace_units_symbol(text, ":Epsilon:", ":Epsilon:", "&Epsilon;")
-    text = _replace_units_symbol(text, ":epsilon:", ":epsilon:", "&epsilon;")
-    text = _replace_units_symbol(text, ":Zeta:", ":Zeta:", "&Zeta;")
-    text = _replace_units_symbol(text, ":zeta:", ":zeta:", "&zeta;")
-    text = _replace_units_symbol(text, ":Eta:", ":Eta:", "&Eta;")
-    text = _replace_units_symbol(text, ":eta:", ":eta:", "&eta;")
-    text = _replace_units_symbol(text, ":Theta:", ":Theta:", "&Theta;")
-    text = _replace_units_symbol(text, ":theta:", ":theta:", "&theta;")
-    text = _replace_units_symbol(text, ":Iota:", ":Iota:", "&Iota;")
-    text = _replace_units_symbol(text, ":iota:", ":iota:", "&iota;")
-    text = _replace_units_symbol(text, ":Kappa:", ":Kappa:", "&Kappa;")
-    text = _replace_units_symbol(text, ":kappa:", ":kappa:", "&kappa;")
-    text = _replace_units_symbol(text, ":Lambda:", ":Lambda:", "&Lambda;")
-    text = _replace_units_symbol(text, ":lambda:", ":lambda:", "&lambda;")
-    text = _replace_units_symbol(text, ":Mu:", ":Mu:", "&Mu;")
-    text = _replace_units_symbol(text, ":mu:", ":mu:", "&mu;")
-    text = _replace_units_symbol(text, ":Nu:", ":Nu:", "&Nu;")
-    text = _replace_units_symbol(text, ":nu:", ":nu:", "&nu;")
-    text = _replace_units_symbol(text, ":Xi:", ":Xi:", "&Xi;")
-    text = _replace_units_symbol(text, ":xi:", ":xi:", "&xi;")
-    text = _replace_units_symbol(text, ":Omicron:", ":Omicron:", "&Omicron;")
-    text = _replace_units_symbol(text, ":omicron:", ":omicron:", "&omicron;")
-    text = _replace_units_symbol(text, ":Pi:", ":Pi:", "&Pi;")
-    text = _replace_units_symbol(text, ":pi:", ":pi:", "&pi;")
-    text = _replace_units_symbol(text, ":Rho:", ":Rho:", "&Rho;")
-    text = _replace_units_symbol(text, ":rho:", ":rho:", "&rho;")
-    text = _replace_units_symbol(text, ":Sigma:", ":Sigma:", "&Sigma;")
-    text = _replace_units_symbol(text, ":sigma:", ":sigma:", "&sigma;")
-    text = _replace_units_symbol(text, ":sigmaf:", ":sigmaf:", "&sigmaf;")
-    text = _replace_units_symbol(text, ":Tau:", ":Tau:", "&Tau;")
-    text = _replace_units_symbol(text, ":tau:", ":tau:", "&tau;")
-    text = _replace_units_symbol(text, ":Upsilon:", ":Upsilon:", "&Upsilon;")
-    text = _replace_units_symbol(text, ":upsilon:", ":upsilon:", "&upsilon;")
-    text = _replace_units_symbol(text, ":Phi:", ":Phi:", "&Phi;")
-    text = _replace_units_symbol(text, ":phi:", ":phi:", "&phi;")
-    text = _replace_units_symbol(text, ":Chi:", ":Chi:", "&Chi;")
-    text = _replace_units_symbol(text, ":chi:", ":chi:", "&chi;")
-    text = _replace_units_symbol(text, ":Psi:", ":Psi:", "&Psi;")
-    text = _replace_units_symbol(text, ":psi:", ":psi:", "&psi;")
-    text = _replace_units_symbol(text, ":Omega:", ":Omega:", "&Omega;")
-    text = _replace_units_symbol(text, ":omega:", ":omega:", "&omega;")
+
+    # Loop through the dictionary of units symbols and replace them with their HTML entities
+    for key, value in UNITS_SYMBOLS_HTML.items():
+        text = _replace_units_symbol(text, key, key, value)
 
     return text
+
+
+UNITS_SYMBOLS_HTML = {
+    "degC": "&deg;C",
+    "degF": "&deg;F",
+    ":pm:": "&plusmn;",
+    ":mp:": "&mnplus;",
+    ":lt:": "&lt;",
+    ":gt:": "&gt;",
+    ":le:": "&le;",
+    ":ge:": "&ge;",
+    ":cdot:": "&sdot;",
+    ":times:": "&times;",
+    ":div:": "&divide;",
+    ":ne:": "&ne;",
+    ":prime:": "&prime;",
+    ":rightarrow:": "&rarr;",
+    ":leftarrow:": "&larr;",
+    ":micro:": "&micro;",
+    ":mu:": "&micro;",
+    ":ohm:": "&#8486;",
+    ":angstrom:": "&#8491;",
+    ":times:": "&times;",
+    ":plusminus:": "&plusmn;",
+    ":permil:": "&permil;",
+    ":permille:": "&permil;",
+    ":degree:": "&deg;",
+    ":degrees:": "&deg;",
+    ":space:": "&nbsp;",
+    ":Alpha:": "&Alpha;",
+    ":alpha:": "&alpha;",
+    ":Beta:": "&Beta;",
+    ":beta:": "&beta;",
+    ":Gamma:": "&Gamma;",
+    ":gamma:": "&gamma;",
+    ":Delta:": "&Delta;",
+    ":delta:": "&delta;",
+    ":Epsilon:": "&Epsilon;",
+    ":epsilon:": "&epsilon;",
+    ":varepsilon:": "&varepsilon;",
+    ":Zeta:": "&Zeta;",
+    ":zeta:": "&zeta;",
+    ":Eta:": "&Eta;",
+    ":eta:": "&eta;",
+    ":Theta:": "&Theta;",
+    ":theta:": "&theta;",
+    ":vartheta:": "&vartheta;",
+    ":Iota:": "&Iota;",
+    ":iota:": "&iota;",
+    ":Kappa:": "&Kappa;",
+    ":kappa:": "&kappa;",
+    ":Lambda:": "&Lambda;",
+    ":lambda:": "&lambda;",
+    ":Mu:": "&Mu;",
+    ":mu:": "&mu;",
+    ":Nu:": "&Nu;",
+    ":nu:": "&nu;",
+    ":Xi:": "&Xi;",
+    ":xi:": "&xi;",
+    ":Omicron:": "&Omicron;",
+    ":omicron:": "&omicron;",
+    ":Pi:": "&Pi;",
+    ":pi:": "&pi;",
+    ":Rho:": "&Rho;",
+    ":rho:": "&rho;",
+    ":Sigma:": "&Sigma;",
+    ":sigma:": "&sigma;",
+    ":sigmaf:": "&sigmaf;",
+    ":varsigma:": "&varsigma;",
+    ":Tau:": "&Tau;",
+    ":tau:": "&tau;",
+    ":Upsilon:": "&Upsilon;",
+    ":upsilon:": "&upsilon;",
+    ":Phi:": "&Phi;",
+    ":phi:": "&phi;",
+    ":Chi:": "&Chi;",
+    ":chi:": "&chi;",
+    ":Psi:": "&Psi;",
+    ":psi:": "&psi;",
+    ":Omega:": "&Omega;",
+    ":omega:": "&omega;",
+}
 
 
 def define_units(units_notation: str) -> UnitDefinitionList:
