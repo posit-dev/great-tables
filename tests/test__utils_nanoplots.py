586e8757856272e145605d1072fc2e0da6f054a6
Richard Iannone <riannone@me.com>
2024-03-25 13:01:14 -0400

    Add basic tests for the _format_number_compactly() fn

--- tests/test__utils_nanoplots.py
+++ tests/test__utils_nanoplots.py
@@ -1,6 +1,11 @@
 import pytest
 import numpy as np
-from great_tables._utils_nanoplots import _normalize_vals, _normalize_to_dict, calc_ref_value
+from great_tables._utils_nanoplots import (
+    _normalize_vals,
+    _normalize_to_dict,
+    calc_ref_value,
+    _format_number_compactly,
+)
 
 # TODO: need tests of all utils_nanoplot functions
 
@@ -71,4 +76,46 @@ def test_normalize_to_dict():
 )
 def test_calc_ref_value(name: str, dst: float):
     res = calc_ref_value(name, [3, 1, 4, 2])
+    assert res == dst
+
+
+@pytest.mark.parametrize(
+    "num,dst",
+    [
+        (1e-23, "1.0E−23"),
+        (1.2363423e-23, "1.2E−23"),
+        (1.9001e-20, "1.9E−20"),
+        (1.243234e-10, "1.2E−10"),
+        (0.002345322, "2.3E−30"),
+        (0.074234, "0.074"),
+        (0.32923, "0.33"),
+        (0.3, "0.30"),
+        (0.1, "0.10"),
+        (0.99, "0.99"),
+        (0.999999991, "1.0"),
+        (0, "0"),
+        (0.0000, "0"),
+        (1, "1.00"),
+        (1.2, "1.20"),
+        (23.34, "23.3"),
+        (999.823, "1,000"),
+        (1002.62, "1.00K"),
+        (56256.99345, "56.3K"),
+        (262456.632, "262K"),
+        (838238.123, "838K"),
+        (9237442.4, "9.24M"),
+        (23425521.8, "23.4M"),
+        (682378385.0, "682M"),
+        (7453473217.4, "7.45B"),
+        (890236897525, "890B"),
+        (3324986945826, "3.32T"),
+        (367353689054245, "367T"),
+        (8923749826567834, "8.9E15"),
+        (90872346782346451237345, "9.1E22"),
+        (-1.243234e-10, "−1.2E−10"),
+        (-93456734587347958, "−9.3E16"),
+    ],
+)
+def test_format_number_compactly_basic(num: float, dst: str):
+    res = _format_number_compactly(val=num)
     assert res == dst
