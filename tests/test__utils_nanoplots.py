fc490e414a3c34e5800abe7b585a635dcccdf31a
Richard Iannone <riannone@me.com>
2024-03-28 16:24:24 -0400

    Make test corrections for _normalize_option_list()

--- tests/test__utils_nanoplots.py
+++ tests/test__utils_nanoplots.py
@@ -125,33 +125,32 @@ def test_is_integerlike():
 # TODO: add tests for _remove_na_from_list()
 
 
-# def test_normalize_option_list():
-#     # Test case 1: Normalization with no missing values
-#     x = [1, 2, 3, 4, 5]
-#     expected_output = [0.0, 0.25, 0.5, 0.75, 1.0]
-#     assert _normalize_option_list(x) == expected_output
-#
-#     # Test case 2: Normalization with missing values
-#     x = [1, np.nan, 3, 4, np.nan]
-#     expected_output = [0.0, np.nan, 0.6666666666666666, 1.0, np.nan]
-#     assert _normalize_option_list(x) == expected_output
-#
-#     # Test case 3: Normalization with all missing values
-#     x = [np.nan, np.nan, np.nan]
-#     expected_output = [np.nan, np.nan, np.nan]
-#     assert _normalize_option_list(x) == expected_output
-#
-#     # Test case 4: Normalization with negative values
-#     x = [-5, -3, -1, 0, 2, 4]
-#     expected_output = [
-#         0.0,
-#         0.2222222222222222,
-#         0.4444444444444444,
-#         0.5555555555555556,
-#         0.7777777777777778,
-#         1.0,
-#     ]
-#     assert _normalize_option_list(x) == expected_output
+def test_normalize_option_list():
+
+    # Test case 1: Normalization with five values
+    x = [1, 2, 3, 4, 5]
+    expected_output = [1, 2, 3, 4, 5]
+    assert _normalize_option_list(x, num_y_vals=5) == expected_output
+
+    # Test case 2: Normalization with missing values
+    x = [1, np.nan, 3, 4, np.nan]
+    expected_output = [1, np.nan, 3, 4, np.nan]
+    assert _normalize_option_list(x, num_y_vals=5) == expected_output
+
+    # Test case 3: Normalization with all missing values
+    x = [np.nan, np.nan, np.nan]
+    expected_output = [np.nan, np.nan, np.nan]
+    assert _normalize_option_list(x, num_y_vals=3) == expected_output
+
+    # Test case 4: Normalization of single value in list
+    x = [1]
+    expected_output = [1, 1, 1, 1, 1]
+    assert _normalize_option_list(x, num_y_vals=5) == expected_output
+
+    # Test case 5: Normalization of single value (not in list)
+    x = 1
+    expected_output = [1, 1, 1, 1, 1]
+    assert _normalize_option_list(x, num_y_vals=5) == expected_output
 
 
 @pytest.mark.parametrize(
