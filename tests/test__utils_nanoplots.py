a4187b192a129e576bae404fd6681cbc6ab4d7de
Richard Iannone <riannone@me.com>
2024-03-28 16:24:47 -0400

    Mark specific tests as xfailing

--- tests/test__utils_nanoplots.py
+++ tests/test__utils_nanoplots.py
@@ -78,9 +78,10 @@ def test_val_is_numeric():
     assert not _val_is_numeric("a")
 
 
-# def test_val_is_numeric_fails_list_input():
-#    with pytest.raises(ValueError):
-#        _val_is_numeric([1, 2, 3])
+@pytest.mark.xfail
+def test_val_is_numeric_fails_list_input():
+    with pytest.raises(ValueError):
+        _val_is_numeric([1, 2, 3])
 
 
 def test_val_is_str():
@@ -91,9 +92,10 @@ def test_val_is_str():
     assert not _val_is_str(1)
 
 
-# def test_val_is_str_fails_list_input():
-#    with pytest.raises(ValueError):
-#        _val_is_str(["a", "b", "c"])
+@pytest.mark.xfail
+def test_val_is_str_fails_list_input():
+    with pytest.raises(ValueError):
+        _val_is_str(["a", "b", "c"])
 
 
 # TODO: add tests for _val_is_missing()
