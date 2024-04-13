1528f64998079b604af56299546ca4e02e88bc9d
Jerry Wu <jerry@ycwu.space>
2024-04-13 16:11:44 +0800

    Update test_scss.py

--- tests/test_scss.py
+++ tests/test_scss.py
@@ -6,21 +6,23 @@ from great_tables._scss import font_color, css_add, compile_scss
 
 
 @pytest.mark.parametrize(
-    "src,dst",
+    "src,dst,table_font_color,table_font_color_light",
     [
-        ("#FFFFFF", "#000000"),
-        ("#000000", "#FFFFFF"),
-        ("white", "#000000"),
-        ("black", "#FFFFFF"),
-        ("silver", "#000000"),
-        ("transparent", "#000000"),
-        ("currentcolor", "currentcolor"),
-        ("currentColor", "currentcolor"),
+        ("#FFFFFF", "#000000", "#000000", "#FFFFFF"),
+        ("#000000", "#FFFFFF", "black", "white"),
+        ("white", "#000000", "#000000", "#FFFFFF"),
+        ("black", "#FFFFFF", "black", "white"),
+        ("silver", "#000000", "#000000", "#FFFFFF"),
+        ("transparent", "#000000", "#000000", "#FFFFFF"),
+        ("transparent", "black", "black", "white"),
+        ("currentcolor", "currentcolor", "#000000", "#FFFFFF"),
+        ("currentColor", "currentcolor", "black", "white"),
     ],
 )
-def test_font_color(src, dst):
-    res = font_color(src, "#000000", "#FFFFFF")
+def test_font_color(src, dst, table_font_color, table_font_color_light):
+    res = font_color(src, table_font_color, table_font_color_light)
     assert res == dst
+
 
 
 @pytest.mark.parametrize(
