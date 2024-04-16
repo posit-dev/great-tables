abdcaef36eaeb82d4952f185a5bc87545668098a
Michael Chow <mc_al_github@fastmail.com>
2024-04-16 13:38:37 -0400

    tests: update font_color tests to always expect hex

--- tests/test_scss.py
+++ tests/test_scss.py
@@ -6,22 +6,33 @@ from great_tables._scss import font_color, css_add, compile_scss
 
 
 @pytest.mark.parametrize(
-    "src,dst,table_font_color,table_font_color_light",
+    "src,dst",
     [
-        ("#FFFFFF", "#000000", "#000000", "#FFFFFF"),
-        ("#000000", "#FFFFFF", "black", "white"),
-        ("white", "#000000", "#000000", "#FFFFFF"),
-        ("black", "#FFFFFF", "black", "white"),
-        ("silver", "#000000", "#000000", "#FFFFFF"),
-        ("transparent", "#000000", "#000000", "#FFFFFF"),
-        ("transparent", "black", "black", "white"),
-        ("currentcolor", "currentcolor", "#000000", "#FFFFFF"),
-        ("currentColor", "currentcolor", "black", "white"),
+        ("#FFFFFF", "#000000"),
+        ("#000000", "#FFFFFF"),
+        ("white", "#000000"),
+        ("black", "#FFFFFF"),
+        ("silver", "#000000"),
+        ("transparent", "#000000"),
+        ("currentcolor", "currentcolor"),
+        ("currentColor", "currentcolor"),
     ],
 )
-def test_font_color(src, dst, table_font_color, table_font_color_light):
-    res = font_color(src, table_font_color, table_font_color_light)
+def test_font_color(src, dst):
+    res = font_color(src, "#000000", "#FFFFFF")
     assert res == dst
+
+
+@pytest.mark.parametrize(
+    "src, dst",
+    [
+        ("transparent", "#FFFFFF"),
+        ("currentColor", "currentcolor"),
+        ("black", "#FFFFFF"),
+    ],
+)
+def test_font_color_normalizes_table_color_names(src, dst):
+    assert font_color(src, "black", "white") == dst
 
 
 @pytest.mark.parametrize(
