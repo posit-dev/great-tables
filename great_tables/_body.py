cdaa6700761b8040c90ce619e9d573a905e03256
Jerry Wu <jerry@ycwu.space>
2024-04-16 21:56:06 +0800

    Update _body.py
    fix black

--- great_tables/_body.py
+++ great_tables/_body.py
@@ -10,10 +10,10 @@ if TYPE_CHECKING:
 
 
 def body_reassemble(body: Body, row_groups: RowGroups, stub_df: Stub, boxhead: Boxhead) -> Body:
-    #cols = [col_info.var for col_info in boxhead]
+    # cols = [col_info.var for col_info in boxhead]
 
-    #start_final = get_row_reorder_df(row_groups, stub_df)
-    #rows = [final for _, final in start_final]
+    # start_final = get_row_reorder_df(row_groups, stub_df)
+    # rows = [final for _, final in start_final]
 
     # TODO: once body is just a DataFrame, we can call reorder directly on it
     # return body.__class__(reorder(body.body, rows, cols))
