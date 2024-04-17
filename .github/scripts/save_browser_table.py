c5e4c5788ce2d68b561060ee38d1657416ed418a
Michael Chow <mc_al_github@fastmail.com>
2024-04-17 16:10:20 -0400

    ci: upload chrome image saves as artifact

--- .github/scripts/save_browser_table.py
+++ .github/scripts/save_browser_table.py
@@ -1,3 +1,8 @@
 from great_tables import GT, exibble
+from pathlib import Path
 
-GT(exibble).save("exibble_firefox.png", web_driver="firefox")
+p = Path("_browser_save")
+p.mkdir(exist_ok=True)
+
+for scale in (0.5, 1.0, 2.0):
+    GT(exibble).save(str(p / f"exibble_chrome_{scale}.png"), web_driver="chrome", scale=scale)
