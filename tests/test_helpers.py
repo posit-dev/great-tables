b5721c7738ad643648c6703913b1915e3998c4f2
jrycw <jerry@ycwu.space>
2024-05-16 10:10:02 +0800

    Code review changes

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,10 +1,10 @@
-from great_tables._helpers import LETTERS, letters, pct, px, random_id, _get_font_stack
+from great_tables._helpers import LETTERS, letters, pct, px, random_id, _get_font_stack, FONT_STACKS
 import pytest
 
 
 @pytest.fixture
 def font_stack_names():
-    yield [
+    yield {
         "system-ui",
         "transitional",
         "old-style",
@@ -20,7 +20,7 @@ def font_stack_names():
         "antique",
         "didone",
         "handwritten",
-    ]
+    }
 
 
 def test_px():
@@ -62,7 +62,7 @@ def test_uppercases():
     assert set(bad_letters).difference(uppercases)
 
 
-def test_get_font_stack_raises(font_stack_names):
+def test_get_font_stack_raises():
     name = "fake_name"
     with pytest.raises(ValueError) as exc_info:
         _get_font_stack(name)
@@ -70,134 +70,14 @@ def test_get_font_stack_raises(font_stack_names):
     assert f"Invalid font stack name: {name}" in exc_info.value.args[0]
 
 
-@pytest.mark.parametrize(
-    "name,font_stack ",
-    [
-        ("system-ui", ["system-ui", "sans-serif"]),
-        ("transitional", ["Charter", "Bitstream Charter", "Sitka Text", "Cambria", "serif"]),
-        ("old-style", ["Iowan Old Style", "Palatino Linotype", "URW Palladio L", "P052", "serif"]),
-        (
-            "humanist",
-            [
-                "Seravek",
-                "Gill Sans Nova",
-                "Ubuntu",
-                "Calibri",
-                "DejaVu Sans",
-                "source-sans-pro",
-                "sans-serif",
-            ],
-        ),
-        (
-            "geometric-humanist",
-            [
-                "Avenir",
-                "Montserrat",
-                "Corbel",
-                "URW Gothic",
-                "source-sans-pro",
-                "sans-serif",
-            ],
-        ),
-        ("classical-humanist", ["Optima", "Candara", "Noto Sans", "source-sans-pro", "sans-serif"]),
-        (
-            "neo-grotesque",
-            [
-                "Inter",
-                "Roboto",
-                "Helvetica Neue",
-                "Arial Nova",
-                "Nimbus Sans",
-                "Arial",
-                "sans-serif",
-            ],
-        ),
-        ("monospace-slab-serif", ["Nimbus Mono PS", "Courier New", "monospace"]),
-        (
-            "monospace-code",
-            [
-                "ui-monospace",
-                "Cascadia Code",
-                "Source Code Pro",
-                "Menlo",
-                "Consolas",
-                "DejaVu Sans Mono",
-                "monospace",
-            ],
-        ),
-        (
-            "industrial",
-            [
-                "Bahnschrift",
-                "DIN Alternate",
-                "Franklin Gothic Medium",
-                "Nimbus Sans Narrow",
-                "sans-serif-condensed",
-                "sans-serif",
-            ],
-        ),
-        (
-            "rounded-sans",
-            [
-                "ui-rounded",
-                "Hiragino Maru Gothic ProN",
-                "Quicksand",
-                "Comfortaa",
-                "Manjari",
-                "Arial Rounded MT",
-                "Arial Rounded MT Bold",
-                "Calibri",
-                "source-sans-pro",
-                "sans-serif",
-            ],
-        ),
-        (
-            "slab-serif",
-            [
-                "Rockwell",
-                "Rockwell Nova",
-                "Roboto Slab",
-                "DejaVu Serif",
-                "Sitka Small",
-                "serif",
-            ],
-        ),
-        (
-            "antique",
-            [
-                "Superclarendon",
-                "Bookman Old Style",
-                "URW Bookman",
-                "URW Bookman L",
-                "Georgia Pro",
-                "Georgia",
-                "serif",
-            ],
-        ),
-        (
-            "didone",
-            [
-                "Didot",
-                "Bodoni MT",
-                "Noto Serif Display",
-                "URW Palladio L",
-                "P052",
-                "Sylfaen",
-                "serif",
-            ],
-        ),
-        (
-            "handwritten",
-            ["Segoe Print", "Bradley Hand", "Chilanka", "TSCu_Comic", "casual", "cursive"],
-        ),
-    ],
-)
-def test_get_font_stack_add_emoji_false(name, font_stack):
-    assert _get_font_stack(name, add_emoji=False) == font_stack
+def test_get_font_stack_add_emoji_false(font_stack_names):
+    assert all(
+        _get_font_stack(name, add_emoji=False) == FONT_STACKS[name] for name in font_stack_names
+    )
 
 
 def test_get_font_stack_add_emoji_true(font_stack_names):
-    extended_emoji = set(
-        ["Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"]
+    extended_emoji = {"Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"}
+    assert all(
+        extended_emoji.issubset(_get_font_stack(name, add_emoji=True)) for name in font_stack_names
     )
-    assert all(extended_emoji.issubset(_get_font_stack(name)) for name in font_stack_names)
