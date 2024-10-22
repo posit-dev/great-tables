40da213d9bb68b8ba399c4f982e4975f489801d3
Richard Iannone <riannone@me.com>
2024-10-21 20:28:48 -0400

    Add tests for Text, Md, Html class methods

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,9 +1,26 @@
 from __future__ import annotations
 
-from great_tables._text import _latex_escape, escape_pattern_str_latex
+from great_tables._text import Text, Md, Html, _latex_escape, escape_pattern_str_latex
 
 
-def test_process_text_latex():
+def test_text_class():
+
+    assert Text("<p>Some Text</p>").to_html() == "<p>Some Text</p>"
+    assert Text("__Some Text__").to_latex() == "__Some Text__"
+
+
+def test_md_class():
+
+    assert Md("**text**").to_html() == "<strong>text</strong>"
+
+
+def test_html_class():
+
+    assert Html("<strong>text</strong>").to_html() == "<strong>text</strong>"
+    assert Html("<strong>text</strong>").to_latex() == "<strong>text</strong>"
+
+
+def test_latex_escape():
 
     assert _latex_escape("a & b") == "a \\& b"
     assert _latex_escape("a & b & c") == "a \\& b \\& c"
