4fb78cf79d2423e948e56f438b43db82360471a4
Richard Iannone <riannone@me.com>
2024-10-21 20:51:23 -0400

    Add tests for `_process_text()`

--- tests/test_text.py
+++ tests/test_text.py
@@ -1,6 +1,13 @@
 from __future__ import annotations
 
-from great_tables._text import Text, Md, Html, _latex_escape, escape_pattern_str_latex
+from great_tables._text import (
+    Text,
+    Md,
+    Html,
+    _latex_escape,
+    escape_pattern_str_latex,
+    _process_text,
+)
 
 
 def test_text_class():
@@ -32,3 +39,20 @@ def test_escape_pattern_str_latex():
     assert escape_pattern_str_latex("{x}") == "{x}"
     assert escape_pattern_str_latex("a $_{1} %ab {2}") == "a \\$\\_{1} \\%ab {2}"
     assert escape_pattern_str_latex("a{b}c") == "a\\{b\\}c"
+
+
+def test_process_text_html():
+
+    assert _process_text("a & <b>", context="html") == "a &amp; &lt;b&gt;"
+    assert _process_text(Text("a & <b>"), context="html") == "a & <b>"
+    assert _process_text(Md("**a** & <b>"), context="html") == "<strong>a</strong> &amp; <b>"
+    assert _process_text(Html("**a** & <b>"), context="html") == "**a** & <b>"
+    assert _process_text(None, context="html") == ""
+
+
+def test_process_text_latex():
+
+    assert _process_text("a & _b_", context="latex") == "a \\& \\_b\\_"
+    assert _process_text(Text("\\_\\$"), context="latex") == "\\_\\$"
+    assert _process_text(Html("**a** & <b>"), context="latex") == "**a** & <b>"
+    assert _process_text(None, context="latex") == ""
