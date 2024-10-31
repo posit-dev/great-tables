5b9cb577e861bdf3f56e617672e0a7881b64f81d
Richard Iannone <riannone@me.com>
2024-10-31 11:06:30 -0400

    Add docstring to `is_quarto_render()`

--- great_tables/quarto.py
+++ great_tables/quarto.py
@@ -2,7 +2,12 @@ import os
 
 
 def is_quarto_render() -> bool:
+    """
+    Check if the current environment is a Quarto environment.
 
-    # Check for the presence of the QUARTO_BIN_PATH environment variable
-    # to determine if the current environment is a Quarto environment.
+    This environment variable check is used to determine if there is currently a Quarto
+    render occurring. This is useful for determining if certain rendering options should be
+    enabled or disabled for this specific environment.
+    """
+
     return "QUARTO_BIN_PATH" in os.environ
