fe4a232d115246d13164abfe57ccee1ce59e246f
Richard Iannone <riannone@me.com>
2024-11-20 14:52:46 -0500

    Ensure that `fmt_icon()` is method of GT class

--- great_tables/gt.py
+++ great_tables/gt.py
@@ -17,6 +17,7 @@ from great_tables._formats import (
     fmt_currency,
     fmt_date,
     fmt_datetime,
+    fmt_icon,
     fmt_image,
     fmt_integer,
     fmt_markdown,
@@ -232,6 +233,7 @@ class GT(
     fmt_datetime = fmt_datetime
     fmt_markdown = fmt_markdown
     fmt_image = fmt_image
+    fmt_icon = fmt_icon
     fmt_units = fmt_units
     fmt_nanoplot = fmt_nanoplot
     data_color = data_color
