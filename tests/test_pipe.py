2dafcc77e696a98b3222ed101f9d3b0ac521fe4e
Michael Chow <mc_al_github@fastmail.com>
2024-12-10 14:43:52 -0500

    tests: simplify .pipe test

--- tests/test_pipe.py
+++ tests/test_pipe.py
@@ -1,37 +1,17 @@
 import polars as pl
-from great_tables import GT, loc, style
+
+from great_tables import GT
 
 
 def test_pipe():
     columns = ["x", "y"]
-    colors = ["lightgray", "lightblue"]
-    df = pl.DataFrame(dict(zip(columns, [[1, 2, 3], [3, 2, 1]])))
+    label = "a spanner"
+    df = pl.DataFrame({"x": [1, 2, 3], "y": [3, 2, 1]})
 
-    gt1 = (
-        GT(df)
-        .tab_style(
-            style=style.fill(color=colors[0]),
-            locations=loc.body(
-                columns=columns[0], rows=pl.col(columns[0]).eq(pl.col(columns[0]).max())
-            ),
-        )
-        .tab_style(
-            style=style.fill(color=colors[1]),
-            locations=loc.body(
-                columns=columns[1], rows=pl.col(columns[1]).eq(pl.col(columns[1]).max())
-            ),
-        )
-    )
+    def tab_spanner2(gt, label, columns):
+        return gt.tab_spanner(label=label, columns=columns)
 
-    def tbl_style(gtbl: GT, columns: list[str], colors: list[str]) -> GT:
-        for column, color in zip(columns, colors):
-            gtbl = gtbl.tab_style(
-                style=style.fill(color=color),
-                locations=loc.body(columns=column, rows=pl.col(column).eq(pl.col(column).max())),
-            )
-        return gtbl
+    gt1 = GT(df).tab_spanner(label, columns=columns)
+    gt2 = GT(df).pipe(tab_spanner2, label, columns=columns)
 
-    gt2 = GT(df).pipe(tbl_style, columns, colors)  # check *args
-    gt3 = GT(df).pipe(tbl_style, columns=columns, colors=colors)  # check **kwargs
-
-    assert gt1._styles == gt2._styles == gt3._styles
+    assert len(gt1._spanners) == len(gt2._spanners)
