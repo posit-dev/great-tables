bd638a275a52b7472afa4d510f722adecb0ae1c5
jrycw <jerry@ycwu.space>
2025-03-03 18:53:08 +0800

    Rename `Boxhead._set_cols_visible()` to `Boxhead._set_cols_info()` and update related logic

--- great_tables/_gt_data.py
+++ great_tables/_gt_data.py
@@ -311,17 +311,12 @@ class Boxhead(_Sequence[ColInfo]):
 
         return self.__class__(new_cols)
 
-    def _set_cols_visible(self, colnames: list[str], is_hidden: bool) -> Self:
+    def _set_cols_info(self, colnames: list[str], colinfo_type: ColInfoTypeEnum) -> Self:
         # TODO: validate that colname is in the boxhead
-        if is_hidden:
-            visibility_type = ColInfoTypeEnum.hidden
-        else:
-            visibility_type = ColInfoTypeEnum.default
-
         res: list[ColInfo] = []
         for col in self._d:
             if col.var in colnames:
-                new_col = replace(col, type=visibility_type)
+                new_col = replace(col, type=colinfo_type)
                 res.append(new_col)
             else:
                 res.append(col)
@@ -329,10 +324,10 @@ class Boxhead(_Sequence[ColInfo]):
         return self.__class__(res)
 
     def set_cols_hidden(self, colnames: list[str]) -> Self:
-        return self._set_cols_visible(colnames=colnames, is_hidden=True)
+        return self._set_cols_info(colnames=colnames, colinfo_type=ColInfoTypeEnum.hidden)
 
     def set_cols_unhidden(self, colnames: list[str]) -> Self:
-        return self._set_cols_visible(colnames=colnames, is_hidden=False)
+        return self._set_cols_info(colnames=colnames, colinfo_type=ColInfoTypeEnum.default)
 
     def align_from_data(self, data: TblData) -> Self:
         """Updates align attribute in entries based on data types."""
