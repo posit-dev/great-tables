bb0c28dd00d73e7c7219e17d67e368e5bfade9be
Tyler Riccio <tylerriccio8@gmail.com>
2025-06-01 14:03:15 -0400

    add test mocking pandas not present

--- tests/test__formats_vals.py
+++ tests/test__formats_vals.py
@@ -4,6 +4,7 @@ import pytest
 from great_tables._formats_vals import _make_one_col_table
 from great_tables._tbl_data import to_list
 import sys
+from unittest.mock import patch
 
 
 @pytest.mark.parametrize("src", [1, [1], (1,), pd.Series([1]), pl.Series([1])])
@@ -14,15 +15,10 @@ def test_roundtrip(src):
 
 
 def test_one_column_table_no_lib() -> None:
-    sys.modules["pandas"] = None
+    with patch.dict(sys.modules, {"pandas": None}):
+        with pytest.raises(ModuleNotFoundError):
+            import pandas  # make sure the test is valid
 
-    with pytest.raises(ModuleNotFoundError):
-        import pandas
+        vals = [1, 2, 3]
 
-    vals = [1, 2, 3]
-
-    _make_one_col_table(vals=vals)
-    raise
-
-
-test_one_column_table_no_lib()
+        _make_one_col_table(vals=vals)
