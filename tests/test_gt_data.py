004b1097b5cc68a29c6ae078ec2634d07580429b
Richard Iannone <riannone@me.com>
2025-08-06 11:01:14 -0400

    Add tests for CellMerges class (including errors)

--- tests/test_gt_data.py
+++ tests/test_gt_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import pytest
 from great_tables import GT
-from great_tables._gt_data import Boxhead, ColInfo, RowInfo, Stub
+from great_tables._gt_data import Boxhead, ColInfo, RowInfo, Stub, CellMerges, MergeError
 
 
 def test_stub_construct_df():
@@ -48,3 +49,47 @@ def test_google_font_imports_is_set():
     from great_tables._helpers import GoogleFontImports
 
     assert isinstance(gt_table._google_font_imports, GoogleFontImports)
+
+
+def test_cell_merges_from_data_frame():
+    data = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
+    # Default state where all cells are normal (value `1`)
+    merges = CellMerges(
+        rowspans=[[1, 1, 1], [1, 1, 1], [1, 1, 1]], colspans=[[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    )
+    new_merges = merges.from_data_frame(data)
+
+    assert new_merges.rowspans == [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+    assert new_merges.colspans == [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+
+
+def test_cell_merges_assign_rowspan():
+    # 2-cell rowspan: top-left (0,0) merges with left (1,0)
+    merges = CellMerges(
+        rowspans=[[0, 1, 1], [1, 1, 1], [1, 1, 1]], colspans=[[0, 1, 1], [1, 1, 1], [1, 1, 1]]
+    )
+    new_merges = merges.assign_rowspan(0, 0, 2)
+
+    assert new_merges.rowspans[0][0] == 2
+    assert new_merges.rowspans[1][0] == 0
+
+
+def test_cell_merges_assign_colspan():
+    # 2-cell colspan: top-left (0,0) merges with top (0,1)
+    merges = CellMerges(
+        rowspans=[[0, 1, 1], [1, 1, 1], [1, 1, 1]], colspans=[[0, 1, 1], [1, 1, 1], [1, 1, 1]]
+    )
+    new_merges = merges.assign_colspan(0, 0, 2)
+
+    assert new_merges.colspans[0][0] == 2
+    assert new_merges.colspans[0][1] == 0
+
+
+def test_cell_merges_validate_merge_cell_error_rowspan_not_zero():
+    # Merging cell is already being merged on from the left (cell_rowspans[0] != 0) raises an error
+    merges = CellMerges(
+        rowspans=[[1, 1, 1], [1, 1, 1], [1, 1, 1]], colspans=[[0, 1, 1], [1, 1, 1], [1, 1, 1]]
+    )
+
+    with pytest.raises(MergeError, match="Merging cell is already being merged on from the left"):
+        merges.validate_merge_cell(0, 0, 2)
