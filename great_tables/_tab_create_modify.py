44c86ae31649f0d42c4aa4b7c12b124de10865be
Richard Iannone <riannone@me.com>
2025-08-11 16:54:29 -0400

    Remove unused tab_footnote() function and imports

--- great_tables/_tab_create_modify.py
+++ great_tables/_tab_create_modify.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 from ._helpers import GoogleFont
-from ._locations import Loc, PlacementOptions, set_footnote, set_style
+from ._locations import Loc, set_style
 from ._styles import CellStyle
 
 if TYPE_CHECKING:
@@ -144,42 +144,5 @@ def tab_style(
 
     for loc in locations:
         new_data = set_style(loc, new_data, style)
-
-    return new_data
-
-
-# TODO: note that this function does not yet render, and rendering
-# will likely be implemented down the road (e.g. after basic styling).
-# this is just all the machinery to set data in GT._footnotes
-def tab_footnote(
-    self: GTSelf,
-    footnote: str | list[str],
-    locations: Loc | None | list[Loc | None],
-    placement: PlacementOptions = "auto",
-) -> GTSelf:
-    """Add a footnote to a table
-
-    Parameters
-    ----------
-    footnote
-        The footnote text.
-    locations
-        The location to place the footnote. If None, then a footnote is created without
-        a corresponding marker on the table (TODO: double check this).
-    placement
-        Where to affix the footnote marks to the table content.
-
-    """
-
-    if isinstance(footnote, list):
-        raise NotImplementedError("Currently, only a single string is supported for footnote.")
-
-    if not isinstance(locations, list):
-        locations = [locations]
-
-    new_data = self
-    if isinstance(locations, list):
-        for loc in locations:
-            new_data = set_footnote(loc, self, footnote, placement)
 
     return new_data
