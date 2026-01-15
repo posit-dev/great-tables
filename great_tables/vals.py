550c1bdf54da1f86e6480db41c342f3fc4e76e3e
Richard Iannone <riannone@me.com>
2026-01-15 10:27:54 -0500

    Refactor vals.py imports and update Ruff config

--- great_tables/vals.py
+++ great_tables/vals.py
@@ -3,38 +3,16 @@
 from __future__ import annotations
 
 from ._formats_vals import (
-    val_fmt_bytes as fmt_bytes,
-)
-from ._formats_vals import (
-    val_fmt_currency as fmt_currency,
-)
-from ._formats_vals import (
-    val_fmt_date as fmt_date,
-)
-from ._formats_vals import (
-    val_fmt_engineering as fmt_engineering,
-)
-from ._formats_vals import (
-    val_fmt_image as fmt_image,
-)
-from ._formats_vals import (
-    val_fmt_integer as fmt_integer,
-)
-from ._formats_vals import (
-    val_fmt_markdown as fmt_markdown,
-)
-from ._formats_vals import (
     val_fmt_number as fmt_number,
-)
-from ._formats_vals import (
-    val_fmt_percent as fmt_percent,
-)
-from ._formats_vals import (
-    val_fmt_roman as fmt_roman,
-)
-from ._formats_vals import (
+    val_fmt_integer as fmt_integer,
     val_fmt_scientific as fmt_scientific,
-)
-from ._formats_vals import (
+    val_fmt_engineering as fmt_engineering,
+    val_fmt_percent as fmt_percent,
+    val_fmt_currency as fmt_currency,
+    val_fmt_roman as fmt_roman,
+    val_fmt_bytes as fmt_bytes,
+    val_fmt_date as fmt_date,
     val_fmt_time as fmt_time,
+    val_fmt_markdown as fmt_markdown,
+    val_fmt_image as fmt_image,
 )
