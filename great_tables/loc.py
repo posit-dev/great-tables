61dfb7792dba8bba238930c4a152d6d88c4dc3ad
Richard Iannone <riannone@me.com>
2026-05-31 16:52:44 -0400

    Update loc.py

--- great_tables/loc.py
+++ great_tables/loc.py
@@ -1,36 +1,68 @@
 from __future__ import annotations
 
 from ._locations import (
-    # Header ----
-    LocHeader as header,
-    LocTitle as title,
-    LocSubTitle as subtitle,
-    #
-    # Stubhead ----
-    LocStubhead as stubhead,
-    #
-    # Column Labels ----
-    LocColumnHeader as column_header,
-    LocSpannerLabels as spanner_labels,
-    LocColumnLabels as column_labels,
-    #
-    # Stub ----
-    LocStub as stub,
-    LocRowGroups as row_groups,
-    LocRowGroups as row_group,
-    # LocSummaryStub as summary_stub,
-    LocGrandSummaryStub as grand_summary_stub,
     #
     # Body ----
     LocBody as body,
+)
+from ._locations import (
     #
-    # Summary ----
-    # LocSummary as summary,
-    LocGrandSummary as grand_summary,
+    # Column Labels ----
+    LocColumnHeader as column_header,
+)
+from ._locations import (
+    LocColumnLabels as column_labels,
+)
+from ._locations import (
     #
     # Footer ----
     LocFooter as footer,
+)
+from ._locations import (
+    LocGrandSummary as grand_summary,
+)
+from ._locations import (
+    LocGrandSummaryStub as grand_summary_stub,
+)
+from ._locations import (
+    # Header ----
+    LocHeader as header,
+)
+from ._locations import (
+    LocRowGroups as row_group,
+)
+from ._locations import (
+    LocRowGroups as row_groups,
+)
+from ._locations import (
     LocSourceNotes as source_notes,
+)
+from ._locations import (
+    LocSpannerLabels as spanner_labels,
+)
+from ._locations import (
+    #
+    # Stub ----
+    LocStub as stub,
+)
+from ._locations import (
+    #
+    # Stubhead ----
+    LocStubhead as stubhead,
+)
+from ._locations import (
+    LocSubTitle as subtitle,
+)
+from ._locations import (
+    #
+    # Summary ----
+    LocSummary as summary,
+)
+from ._locations import (
+    LocSummaryStub as summary_stub,
+)
+from ._locations import (
+    LocTitle as title,
 )
 
 __all__ = (
@@ -44,10 +76,10 @@ __all__ = (
     "stub",
     "row_groups",
     "row_group",
-    # "summary_stub",
+    "summary_stub",
     "grand_summary_stub",
     "body",
-    # "summary",
+    "summary",
     "grand_summary",
     "footer",
     "source_notes",
